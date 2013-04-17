53459d7d5d4539f87f0cd750c1b607091a3c8df4
Philip Vasilevski <philip.vasilevski@gmail.com>
2013-04-16 23:22:02 -0400

    second commit - Product not exists

---
+++ app/models/gemgento/product.rb
@@ -0,0 +1,8 @@
+module Gemgento
+  class Product < ActiveRecord::Base
+
+    def list
+    end
+  
+  end
+end
--- tool/soap_catalog_product_rename.rb
+++ tool/soap_catalog_product_rename.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'savon'
-client = Savon.client(wsdl: "http://madelineweinrib.mauinewyork.com/api/soap/index?wsdl", log: false)
+client = Savon.client(wsdl: "http://madelineweinrib.mauinewyork.com/api/soap/index?wsdl", log: true)
 response = client.call(:login, message: { :username => 'philip.vasilevski', :apiKey => '857123FHDShfd' })
 if response.success? == false
   puts "login failed"
@@ -8,14 +8,19 @@ if response.success? == false
 end
 session = response.body[:login_response][:login_return];
 response = client.call(:call, message: {:sessionId => session, :resourcePath => 'catalog_product.list'})
- # fetching all products
 if response.success?
-  # listing found products
   response.body[:call_response][:call_return][:item].each_with_index do |product, i|
-    puts "-------------------------------------------"
-    product = product[:item]
-    product.each do |pkey|
-      puts "#{pkey[:key]} -> #{pkey[:value]}"
+    if i == 0
+      product = product[:item]
+      product.each do |p|
+        if p[:key] == 'sku'
+          
+          info_response = client.call(:call, message: {:sessionId => session, :resourcePath => 'catalog_product.info', :productId => p[:value]})
+          
+          puts info_response.inspect
+          
+        end
+      end
     end
   end
 end
