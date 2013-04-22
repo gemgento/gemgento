6023de1450c51a1a0ced488799c7042cd3184d2a
Philip Vasilevski <philip.vasilevski@gmail.com>
2013-04-22 17:22:38 -0400

    tool/soap_catalog_product_rename.rb for mw

--- tool/soap_catalog_product_rename.rb
+++ tool/soap_catalog_product_rename.rb
@@ -10,7 +10,7 @@ session = response.body[:login_response][:login_return];
 response = client.call(:catalog_product_list, message: {:sessionId => session})
 if response.success?
   response.body[:catalog_product_list_response][:store_view][:item].each_with_index do |product, i|
-    info_response = client.call(:catalog_product_info, message: {:sessionId => session, :product => 17569, :productIdentifierType => 'id', :attributes => ['color','design','quality']})
+    info_response = client.call(:catalog_product_info, message: {:sessionId => session, :product => product[:product_id], :productIdentifierType => 'id', :attributes => ['color','design','quality']})
     product_type = info_response.body[:catalog_product_info_response][:info][:type]
     if product_type == 'simple'      
       color = info_response.body[:catalog_product_info_response][:info][:additional_attributes][:item][0][:value]
