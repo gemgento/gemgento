8ff9588d56d9d39828b7991534f092019d9d3672
Philip Vasilevski <philip.vasilevski@gmail.com>
2013-04-22 17:19:20 -0400

    tool/soap_catalog_product_rename.rb for mw

--- tool/soap_catalog_product_rename.rb
+++ tool/soap_catalog_product_rename.rb
@@ -10,12 +10,14 @@ session = response.body[:login_response][:login_return];
 response = client.call(:catalog_product_list, message: {:sessionId => session})
 if response.success?
   response.body[:catalog_product_list_response][:store_view][:item].each_with_index do |product, i|
-    info_response = client.call(:catalog_product_info, message: {:sessionId => session, :product => product[:product_id], :productIdentifierType => 'id'})
-
-    puts info_response.body[:catalog_product_info_response]
-
+    info_response = client.call(:catalog_product_info, message: {:sessionId => session, :product => 17569, :productIdentifierType => 'id', :attributes => ['color','design','quality']})
     product_type = info_response.body[:catalog_product_info_response][:info][:type]
-    if product_type == 'simple'
+    if product_type == 'simple'      
+      color = info_response.body[:catalog_product_info_response][:info][:additional_attributes][:item][0][:value]
+      quality = info_response.body[:catalog_product_info_response][:info][:additional_attributes][:item][1][:value]
+      design = info_response.body[:catalog_product_info_response][:info][:additional_attributes][:item][2][:value]      
+      puts color+' -- '+design+' -- '+quality
+      update_response = client.call(:catalog_product_update, message: {:sessionId => session, :product => 17569, :productIdentifierType => 'id', :productData => {:name => color+' '+design+' '+quality}})      
     end
   end
 end
