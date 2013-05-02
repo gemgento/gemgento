5a82dc968afaf8c944d30ffbf15bf6d79ff91720
Kevin Pheasey <kevin.pheasey@gmail.com>
2013-05-02 15:35:46 -0400

    Use create_call() in place of client.call()

--- app/models/gemgento/product.rb
+++ app/models/gemgento/product.rb
@@ -10,40 +10,37 @@ module Gemgento
     end
 
     def self.fetch_all
-      client = Gemgento::Magento.api_login
-      response = client.call(:catalog_product_list, message: {:sessionId => @session})
-      if response.success?
-        #puts 'response.body=' + response.body[:catalog_product_list_response].inspect
-        response.body[:catalog_product_list_response][:store_view][:item].each_with_index do |product, i|
-          info_response = client.call(:catalog_product_info, message: {:sessionId => @session, :product => product[:product_id], :productIdentifierType => 'id'})
-          #puts info_response.inspect          
-          p = Product.create
-          p.magento_id = info_response.body[:catalog_product_info_response][:info][:product_id]
-          p.magento_type = info_response.body[:catalog_product_info_response][:info][:type]
-          p.name = info_response.body[:catalog_product_info_response][:info][:name]
-          p.url_key = info_response.body[:catalog_product_info_response][:info][:url_key]
-          p.price = info_response.body[:catalog_product_info_response][:info][:price]
-          p.save
+      response = Gemgento::Magento.create_call(:catalog_product_list, { sessionId: @session })
+      #puts 'response.body=' + response.body[:catalog_product_list_response].inspect
+      response.body[:catalog_product_list_response][:store_view][:item].each_with_index do |product, i|
+        info_response = Gemgento::Magento.create_call(:catalog_product_info, { product: product[:product_id], productIdentifierType: 'id' })
+        #puts info_response.inspect
+        p = Product.create
+        p.magento_id = info_response.body[:catalog_product_info_response][:info][:product_id]
+        p.magento_type = info_response.body[:catalog_product_info_response][:info][:type]
+        p.name = info_response.body[:catalog_product_info_response][:info][:name]
+        p.url_key = info_response.body[:catalog_product_info_response][:info][:url_key]
+        p.price = info_response.body[:catalog_product_info_response][:info][:price]
+        p.save
 
-          image_response = client.call(:catalog_product_attribute_media_list, message: {:sessionId => @session, :product => product[:product_id], :productIdentifierType => 'id'})
-          #puts image_response.body.inspect 
-          if image_response.body[:catalog_product_attribute_media_list_response][:result][:item] != nil && 
+        image_response = Gemgento::Magento.create_call(:catalog_product_attribute_media_list, { product: product[:product_id], productIdentifierType: 'id' })
+        #puts image_response.body.inspect
+        if image_response.body[:catalog_product_attribute_media_list_response][:result][:item] != nil &&
 
-            if image_response.body[:catalog_product_attribute_media_list_response][:result][:item].size > 1
-          
-              image_response.body[:catalog_product_attribute_media_list_response][:result][:item].each_with_index do |img, i|            
-                create_asset(img, p)
-              end
+          if image_response.body[:catalog_product_attribute_media_list_response][:result][:item].size > 1
 
-            else
-              img = image_response.body[:catalog_product_attribute_media_list_response][:result][:item]
+            image_response.body[:catalog_product_attribute_media_list_response][:result][:item].each_with_index do |img, i|
               create_asset(img, p)
             end
 
+          else
+            img = image_response.body[:catalog_product_attribute_media_list_response][:result][:item]
+            create_asset(img, p)
           end
+
         end
       end
-    end    
+    end
 
     def self.create_asset(img, p)
       puts 'IMG?: '+img.inspect
--- lib/gemgento.rb
+++ lib/gemgento.rb
@@ -6,22 +6,35 @@ require 'savon'
 module Gemgento
   class Magento
     def self.api_login
-      @api_url = "http://#{Gemgento::Config[:magento][:url]}/api/v#{Gemgento::Config[:magento][:api_version]}_#{Gemgento::Config[:magento][:api_type]}/index?wsdl=1"
-      client = Savon.client(wsdl: @api_url, log: true)
-      if Gemgento::Session.last.nil?        
-        response = client.call(:login, message: { :username => Gemgento::Config[:magento][:username], :apiKey => Gemgento::Config[:magento][:api_key] })
-        if response.success? == false
-          puts "login failed"
-          System.exit(0)
-        end
+      @api_url = "http://#{Gemgento::Config[:magento][:url]}/index.php/api/v#{Gemgento::Config[:magento][:api_version]}_#{Gemgento::Config[:magento][:api_type]}/?wsdl=1"
+      @client = Savon.client(wsdl: @api_url, log: true)
+      if Gemgento::Session.last.nil?
+        response = @client.call(:login, message: { :username => Gemgento::Config[:magento][:username], :apiKey => Gemgento::Config[:magento][:api_key] })
         @session = response.body[:login_response][:login_return];
         s = Gemgento::Session.new
         s.session_id = @session
         s.save
       else
         @session = Gemgento::Session.last.session_id
-      end      
-      return client
+      end
+    end
+
+    def self.create_call(function, message)
+      if !defined? @client
+        Gemgento::Magento.api_login
+      end
+
+      message[:sessionId] = @session
+
+      begin
+        response = @client.call(function, message: message)
+      rescue
+        puts "Call failed - #{function}"
+        exit
+        response = nil
+      end
+
+      return response
     end
   end
 
