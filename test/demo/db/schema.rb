e9e5d37233b1b143834fded10d69aa99280103bb
Kevin Pheasey <kevin.pheasey@gmail.com>
2013-05-02 12:03:05 -0400

    Updated mw.local logins.

--- test/demo/db/schema.rb
+++ test/demo/db/schema.rb
@@ -9,33 +9,33 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130501184617) do
+ActiveRecord::Schema.define(:version => 20130501192533) do
 
-  create_table "gemgento_assets", force: true do |t|
+  create_table "gemgento_assets", :force => true do |t|
     t.integer  "product_id"
     t.string   "type"
     t.string   "url"
     t.integer  "position"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
-  create_table "gemgento_products", force: true do |t|
+  create_table "gemgento_products", :force => true do |t|
     t.integer  "magento_id"
     t.string   "name"
     t.string   "magento_type"
     t.string   "url_key"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.decimal  "price",        precision: 10, scale: 0
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
+    t.decimal  "price",        :precision => 10, :scale => 0
   end
 
-  create_table "gemgento_sessions", force: true do |t|
+  create_table "gemgento_sessions", :force => true do |t|
     t.string   "session_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
