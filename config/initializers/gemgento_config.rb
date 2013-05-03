1d3e3619a70609d60fdccccf25a0d1afccf1c39f
Kevin Pheasey <kevin.pheasey@gmail.com>
2013-05-03 09:28:49 -0400

    Reverted Magento instantiation changes.

--- config/initializers/gemgento_config.rb
+++ config/initializers/gemgento_config.rb
@@ -1,3 +1,2 @@
 yaml = YAML.load(File.read(Rails.root.join('config','gemgento_config.yml')))
 Gemgento::Config = HashWithIndifferentAccess.new(yaml[Rails.env])
-@magento = Gemgento::Magento.new
--- lib/gemgento.rb
+++ lib/gemgento.rb
@@ -7,7 +7,7 @@ module Gemgento
   class Magento
 
     # Log into the Magento API and setup the session and client
-    def api_login
+    def self.api_login
       @api_url = "http://#{Gemgento::Config[:magento][:url]}/index.php/api/v#{Gemgento::Config[:magento][:api_version]}_#{Gemgento::Config[:magento][:api_type]}/?wsdl=1"
       @client = Savon.client(wsdl: @api_url, log: true)
       if Gemgento::Session.last.nil?
@@ -26,7 +26,7 @@ module Gemgento
     # @param [Symbol] function  The API call to make
     # @param [Hash]   message   Call parameters (does not need session)
     # @return [Hash]
-    def create_call(function, message = {})
+    def self.create_call(function, message = {})
       api_login if !defined? @client
 
       message[:sessionId] = @session
