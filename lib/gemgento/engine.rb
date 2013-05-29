174814bf22f79e07035456f95d4b3dc1eba826f7
Kevin <kevin.pheasey@gmail.com>
2013-05-29 16:02:13 -0400

    Make mountable engine

--- lib/gemgento/engine.rb
+++ lib/gemgento/engine.rb
@@ -4,8 +4,8 @@ module Gemgento
   class Engine < ::Rails::Engine
     isolate_namespace Gemgento
     engine_name 'gemgento'
-    
+
     config.autoload_paths += %W(#{config.root}/lib)
-        
+
   end
 end
