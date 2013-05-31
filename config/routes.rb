8f6b932d6fb2f2a696f0c34dc112b92079f55903
Kevin <kevin.pheasey@gmail.com>
2013-05-31 19:07:44 -0400

    Create category route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Gemgento::Engine.routes.draw do
   root :to => "products#index"
-  resources :products
-  match '/shop/:permalink' => 'products#show', via: :get, as: "shop_permalink"
+
+  resources :products, :categories
+
+  match '/shop/:permalink' => 'products#show', via: :get, as: 'shop_permalink'
+  get '/shop/category/:url_key', to: 'categories#show'
 end
