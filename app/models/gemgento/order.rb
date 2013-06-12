1fbf6dd3e934246b8c44644c617ebad5927e87cd
Kevin <kevin.pheasey@gmail.com>
2013-06-12 16:50:02 -0400

    Map basic order information from Magento.

--- app/models/gemgento/order.rb
+++ app/models/gemgento/order.rb
@@ -1,6 +1,101 @@
 module Gemgento
   class Order < ActiveRecord::Base
-    has_many :products
+    belongs_to :store
     belongs_to :user
+    belongs_to :user_group
+    belongs_to :shipping_address, foreign_key: 'shipping_address_id', class_name: 'Address'
+    belongs_to :billing_address, foreign_key: 'billing_address_id', class_name: 'Address'
+
+    def self.index
+      if Order.find(:all).size == 0
+        fetch_all
+      end
+
+      Order.find(:all)
+    end
+
+    def self.fetch_all
+      response = Gemgento::Magento.create_call(:sales_order_list)
+
+      unless response[:result][:item].is_a? Array
+        response[:result][:item] = [response[:result][:item]]
+      end
+
+      response[:result][:item].each do |order|
+        sync_magento_to_local(order)
+      end
+    end
+
+    private
+
+    # Save Magento order to local
+    def self.sync_magento_to_local(source)
+      order = Order.find_or_initialize_by(magento_id: source[:order_id])
+      order.magento_id = source[:order_id]
+      order.store = Store.find_by(magento_id: source[:store_id])
+      order.is_active = source[:is_active]
+      order.user = User.find_by(magento_id: source[:customer_id])
+      order.tax_amount = source[:tax_amount]
+      order.shipping_amount = source[:shipping_amount]
+      order.discount_amount = source[:discount_amount]
+      order.subtotal = source[:subtotal]
+      order.grand_total = source[:grand_total]
+      order.total_paid = source[:total_paid]
+      order.total_refunded = source[:total_refunded]
+      order.total_qty_ordered = source[:total_qty_ordered]
+      order.total_canceled = source[:total_canceled]
+      order.total_invoiced = source[:total_invoiced]
+      order.total_online_refunded = source[:total_online_refunded]
+      order.total_offline_refunded = source[:total_offline_refunded]
+      order.base_tax_amount = source[:base_tax_amount]
+      order.base_shipping_amount = source[:base_shipping_amount]
+      order.base_discount_amount = source[:base_discount_amount]
+      order.base_subtotal = source[:base_subtotal]
+      order.base_grand_total = source[:base_grand_total]
+      order.base_total_paid = source[:base_total_paid]
+      order.base_total_refunded = source[:base_total_refunded]
+      order.base_total_qty_ordered = source[:base_total_qty_ordered]
+      order.base_total_canceled = source[:base_total_canceled]
+      order.base_total_invoiced = source[:base_total_invoiced]
+      order.base_total_online_refunded = source[:base_total_online_refunded]
+      order.base_total_offline_refunded = source[:base_total_offline_refunded]
+      order.billing_address = Address.find_by(magento_id: source[:billing_address_id])
+      order.billing_fname = source[:billing_firstname]
+      order.billing_lname = source[:billing_lastname]
+      order.shipping_address = Address.find_by(source[:shipping_address_id])
+      order.shipping_fname = source[:shipping_firstname]
+      order.shipping_lname = source[:shipping_lastname]
+      order.billing_name = source[:billing_name]
+      order.shipping_name = source[:shipping_name]
+      order.store_to_base_rate = source[:store_to_base_rate]
+      order.store_to_order_rate = source[:store_to_order_rate]
+      order.base_to_global_rate = source[:base_to_global_rate]
+      order.base_to_order_rate = source[:base_to_order_rate]
+      order.weight = source[:weight]
+      order.store_name = source[:store_name]
+      order.remote_ip = source[:remote_ip]
+      order.status = source[:status]
+      order.state = source[:state]
+      order.applied_rule_ids = source[:applied_rule_ids]
+      order.global_currency_code = source[:global_currency_code]
+      order.base_currency_code = source[:base_currency_code]
+      order.store_currency_code = source[:store_currency_code]
+      order.order_currency_code = source[:order_currency_code]
+      order.shipping_method = source[:shipping_method]
+      order.shipping_description = source[:shipping_description]
+      order.customer_email = source[:customer_email]
+      order.customer_firstname = source[:customer_firstname]
+      order.customer_lastname = source[:customer_lastname]
+      order.quote_id = source[:quote_id]
+      order.is_virtual = source[:is_virtual]
+      order.user_group = UserGroup.find_by(magento_id: source[:customer_group_id])
+      order.customer_note_notify = source[:customer_note_notify]
+      order.customer_is_guest = source[:customer_is_guest]
+      order.email_sent = source[:email_sent]
+      order.increment_id = source[:increment_id]
+      order.gift_message_id = source[:gift_message_id]
+      order.gift_message = source[:gift_message]
+      order.save
+    end
   end
 end
--- test/demo/db/schema.rb
+++ test/demo/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130612171525) do
+ActiveRecord::Schema.define(version: 20130612190917) do
 
   create_table "gemgento_addresses", force: true do |t|
     t.integer  "magento_id",                          null: false
@@ -104,6 +104,75 @@ ActiveRecord::Schema.define(version: 20130612171525) do
     t.integer  "quantity",    default: 0,     null: false
     t.boolean  "is_in_stock", default: false, null: false
     t.boolean  "sync_needed", default: true,  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "gemgento_orders", force: true do |t|
+    t.integer  "magento_id",                                           null: false
+    t.integer  "store_id",                                             null: false
+    t.boolean  "is_active"
+    t.integer  "user_id"
+    t.decimal  "tax_amount",                  precision: 12, scale: 4
+    t.decimal  "shipping_amount",             precision: 12, scale: 4
+    t.decimal  "discount_amount",             precision: 12, scale: 4
+    t.decimal  "subtotal",                    precision: 12, scale: 4
+    t.decimal  "grand_total",                 precision: 12, scale: 4
+    t.decimal  "total_paid",                  precision: 12, scale: 4
+    t.decimal  "total_refunded",              precision: 12, scale: 4
+    t.decimal  "total_qty_ordered",           precision: 12, scale: 4
+    t.decimal  "total_canceled",              precision: 12, scale: 4
+    t.decimal  "total_invoiced",              precision: 12, scale: 4
+    t.decimal  "total_online_refunded",       precision: 12, scale: 4
+    t.decimal  "total_offline_refunded",      precision: 12, scale: 4
+    t.decimal  "base_tax_amount",             precision: 12, scale: 4
+    t.decimal  "base_shipping_amount",        precision: 12, scale: 4
+    t.decimal  "base_discount_amount",        precision: 12, scale: 4
+    t.decimal  "base_subtotal",               precision: 12, scale: 4
+    t.decimal  "base_grand_total",            precision: 12, scale: 4
+    t.decimal  "base_total_paid",             precision: 12, scale: 4
+    t.decimal  "base_total_refunded",         precision: 12, scale: 4
+    t.decimal  "base_total_qty_ordered",      precision: 12, scale: 4
+    t.decimal  "base_total_canceled",         precision: 12, scale: 4
+    t.decimal  "base_total_invoiced",         precision: 12, scale: 4
+    t.decimal  "base_total_online_refunded",  precision: 12, scale: 4
+    t.decimal  "base_total_offline_refunded", precision: 12, scale: 4
+    t.integer  "billing_address_id"
+    t.string   "billing_fname"
+    t.string   "billing_lname"
+    t.integer  "shipping_address_id"
+    t.string   "shipping_fname"
+    t.string   "shipping_lname"
+    t.string   "billing_name"
+    t.string   "shipping_name"
+    t.string   "store_to_base_rate"
+    t.string   "store_to_order_rate"
+    t.string   "base_to_global_rate"
+    t.string   "base_to_order_rate"
+    t.decimal  "weight",                      precision: 12, scale: 4
+    t.string   "store_name"
+    t.string   "remote_ip"
+    t.string   "status"
+    t.string   "state"
+    t.string   "applied_rule_ids"
+    t.string   "global_currency_code"
+    t.string   "base_currency_code"
+    t.string   "store_currency_code"
+    t.string   "order_currency_code"
+    t.string   "shipping_method"
+    t.string   "shipping_description"
+    t.string   "customer_email"
+    t.string   "customer_firstname"
+    t.string   "customer_lastname"
+    t.string   "quote_id"
+    t.boolean  "is_virtual"
+    t.integer  "user_group_id"
+    t.string   "customer_note_notify"
+    t.boolean  "customer_is_guest"
+    t.boolean  "email_sent"
+    t.integer  "increment_id"
+    t.string   "gift_message_id"
+    t.string   "gift_message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
