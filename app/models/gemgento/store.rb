fbb006c275f96d376480ce92031ab409b0c8d680
Kevin <kevin.pheasey@gmail.com>
2013-06-14 19:48:39 -0400

    Move Store mapping to API module.

--- app/models/gemgento/store.rb
+++ app/models/gemgento/store.rb
@@ -12,30 +12,8 @@ module Gemgento
     end
 
     def self.fetch_all
-      response = Gemgento::Magento.create_call(:store_list)
 
-      unless response[:stores][:item].is_a? Array
-        response[:stores][:item] = [response[:stores][:item]]
-      end
 
-      response[:stores][:item].each do |store|
-        sync_magento_to_local(store)
-      end
-
-    end
-
-    private
-
-    # Save Magento store to local
-    def self.sync_magento_to_local(source)
-      store = Store.find_or_initialize_by(magento_id: source[:store_id])
-      store.magento_id = source[:store_id]
-      store.code = source[:code]
-      store.group_id = source[:group_id]
-      store.name = source[:name]
-      store.sort_order = source[:sort_order]
-      store.is_active = source[:is_active]
-      store.save
     end
   end
 end
