142e51e0e6d0aa38806dd97c63bf5504bfe91a60
Kevin <kevin.pheasey@gmail.com>
2013-06-17 11:34:33 -0400

    Move Country mapping to API module.

---
+++ app/models/gemgento/api/soap/directory/country.rb
@@ -0,0 +1,40 @@
+module Gemgento
+  module API
+    module SOAP
+      module Directory
+        class Country
+
+          def self.fetch_all
+            list.each do |country|
+              sync_magento_to_local(country)
+            end
+
+          end
+
+          def self.list
+            response = Gemgento::Magento.create_call(:directory_country_list)
+
+            unless response[:countries][:item].is_a? Array
+              response[:countries][:item] = [response[:countries][:item]]
+            end
+
+            response[:countries][:item]
+          end
+
+          private
+
+          # Save Magento product attribute set to local
+          def self.sync_magento_to_local(source)
+            country = Gemgento.Country.find_or_initialize_by(magento_id: source[:country_id])
+            country.magento_id = source[:country_id]
+            country.iso2_code = source[:iso2_code]
+            country.iso3_code = source[:iso3_code]
+            country.name = source[:name]
+            country.save
+          end
+
+        end
+      end
+    end
+  end
+end
--- app/models/gemgento/country.rb
+++ app/models/gemgento/country.rb
@@ -4,35 +4,11 @@ module Gemgento
 
     def self.index
       if Country.find(:all).size == 0
-        fetch_all
+        API::SOAP::Directory::Country.fetch_all
       end
 
       Country.find(:all)
     end
 
-    def self.fetch_all
-      response = Gemgento::Magento.create_call(:directory_country_list)
-
-      unless response[:countries][:item].is_a? Array
-        response[:countries][:item] = [response[:countries][:item]]
-      end
-
-      response[:countries][:item].each do |country|
-        sync_magento_to_local(country)
-      end
-
-    end
-
-    private
-
-    # Save Magento product attribute set to local
-    def self.sync_magento_to_local(source)
-      country = Country.find_or_initialize_by(magento_id: source[:country_id])
-      country.magento_id = source[:country_id]
-      country.iso2_code = source[:iso2_code]
-      country.iso3_code = source[:iso3_code]
-      country.name = source[:name]
-      country.save
-    end
   end
 end
