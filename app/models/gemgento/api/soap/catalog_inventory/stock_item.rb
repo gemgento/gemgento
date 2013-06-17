6a16c5c0c623bd99bdf6241925edf68f77ea9b71
Kevin <kevin.pheasey@gmail.com>
2013-06-17 14:52:19 -0400

    Fix Inventory.fetch_all

--- app/models/gemgento/api/soap/catalog_inventory/stock_item.rb
+++ app/models/gemgento/api/soap/catalog_inventory/stock_item.rb
@@ -6,24 +6,24 @@ module Gemgento
 
           def self.fetch_all
             products_ids = []
-            Product.find(:all).each do |product|
+            Gemgento::Product.all.each do |product|
               products_ids << product.magento_id
             end
 
-            list(product_ids).each do |inventory|
+            list(products_ids).each do |inventory|
                 sync_magento_to_local(inventory)
             end
           end
 
           def self.list(product_ids)
-            response = Gemgento::Magento.create_call(:catalog_inventory_stock_item_list, { products: product_ids })
+            response = Gemgento::Magento.create_call(:catalog_inventory_stock_item_list, { products: { item: product_ids }})
 
-            unless response[:result][:item].nil?
-              unless response[:result][:item].is_a? Array
-                response[:result][:item] = [response[:result][:item]]
-              end
-            else
+            if response[:result][:item].nil?
               response[:result][:item] = []
+            end
+
+            unless response[:result][:item].is_a? Array
+              response[:result][:item] = [response[:result][:item]]
             end
 
             response[:result][:item]
