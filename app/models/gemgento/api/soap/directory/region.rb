0e740feb807b6bcc78b7f072879f0d6445201eab
Kevin <kevin.pheasey@gmail.com>
2013-06-17 11:37:20 -0400

    Move Region mapping to API module.

---
+++ app/models/gemgento/api/soap/directory/region.rb
@@ -0,0 +1,45 @@
+module Gemgento
+  module API
+    module SOAP
+      module Directory
+        class Region
+
+          def self.fetch_all
+            Gemgento::Country.find(:all).each do |country|
+              list.each do |region|
+                  sync_magento_to_local(region, country)
+              end
+            end
+          end
+
+          def self.list
+            response = Gemgento::Magento.create_call(:directory_region_list, { country: country.iso2_code })
+
+            if !response[:countries][:item].nil?
+              unless response[:countries][:item].is_a? Array
+                response[:countries][:item] = [response[:countries][:item]]
+              end
+            else
+              response[:countries][:item] = []
+            end
+
+            response[:countries][:item]
+          end
+
+          private
+
+          # Save Magento product attribute set to local
+          def self.sync_magento_to_local(source, country)
+            region = Gemgento::Region.find_or_initialize_by(magento_id: source[:region_id])
+            region.magento_id = source[:region_id]
+            region.code = source[:code]
+            region.name = source[:name]
+            region.country = country
+            region.save
+          end
+
+        end
+      end
+    end
+  end
+end
--- app/models/gemgento/region.rb
+++ app/models/gemgento/region.rb
@@ -4,41 +4,11 @@ module Gemgento
 
     def self.index
       if Region.find(:all).size == 0
-        fetch_all
+        API::SOAP::Directory::Region.fetch_all
       end
 
       Region.find(:all)
     end
 
-    def self.fetch_all
-      Country.find(:all).each do |country|
-        message = {
-            country: country.iso2_code
-        }
-        response = Gemgento::Magento.create_call(:directory_region_list, message)
-
-        if !response[:countries][:item].nil?
-          unless response[:countries][:item].is_a? Array
-            response[:countries][:item] = [response[:countries][:item]]
-          end
-
-          response[:countries][:item].each do |region|
-            sync_magento_to_local(region, country)
-          end
-        end
-      end
-    end
-
-    private
-
-    # Save Magento product attribute set to local
-    def self.sync_magento_to_local(source, country)
-      region = Region.find_or_initialize_by(magento_id: source[:region_id])
-      region.magento_id = source[:region_id]
-      region.code = source[:code]
-      region.name = source[:name]
-      region.country = country
-      region.save
-    end
   end
 end
