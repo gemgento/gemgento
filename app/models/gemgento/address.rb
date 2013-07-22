9ff49eb43200502c486649e130f91f1873430436
Kevin <kevin.pheasey@gmail.com>
2013-07-22 11:29:50 -0400

    Address validations

--- app/models/gemgento/address.rb
+++ app/models/gemgento/address.rb
@@ -5,10 +5,12 @@ module Gemgento
     belongs_to :region
     belongs_to :order
 
-    validates :fname, length: { minimum: 2 }
-    validates :lname, length: { minimum: 2 }
-    validates :street, length: { minimum: 2 }
+    validates :fname, presence: true, message: 'First name is required'
+    validates :lname, presence: true, message: 'Last name is required'
+    validates :street, presence: true, message: 'Address is required'
     validates :country, presence: true
+    validates :postcode, presence: true, message: 'Postal code is required'
+    validates :telephone, presence: true, message: 'Phone number is required'
 
     attr_accessor :address1, :address2, :address3
 
