436e0b6d7ef3360b935027debf7270248dbccc51
Kevin <kevin.pheasey@gmail.com>
2013-07-23 09:53:48 -0400

    Break Magento street attribute into address1/2/3 after loading a record and combine them before validating a record.

--- app/models/gemgento/address.rb
+++ app/models/gemgento/address.rb
@@ -14,6 +14,10 @@ module Gemgento
 
     attr_accessor :address1, :address2, :address3
 
+    after_find  :explode_street_address
+    before_validation :implode_street_address
+
+
     def self.index
       if Address.find(:all).size == 0
         API::SOAP::Customer::Address.fetch_all
@@ -22,5 +26,19 @@ module Gemgento
       Address.find(:all)
     end
 
+    private
+
+    def explode_street_address
+      address = self.street.split("\n")
+      self.address1 = address[0] unless address[0].nil?
+      self.address2 = address[1] unless address[1].nil?
+      self.address3 = address[2] unless address[2].nil?
+    end
+
+    def implode_street_address
+      self.street = self.address1 unless self.address1.nil?
+      self.street = "#{self.street}\n#{self.address2}" unless self.address2.nil?
+      self.street = "#{self.street}\n#{self.address3}" unless self.address3.nil?
+    end
   end
 end
