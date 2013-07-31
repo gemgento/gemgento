31b0f59f4bed5780fc4e35ee22038b89ee0595b9
Kevin <kevin.pheasey@gmail.com>
2013-07-31 17:47:09 -0400

    Allow guest checkout.

--- app/models/gemgento/api/soap/sales/order.rb
+++ app/models/gemgento/api/soap/sales/order.rb
@@ -57,7 +57,7 @@ module Gemgento
 
           # Save Magento order to local
           def self.sync_magento_to_local(source)
-            order = Gemgento::Order.where(order_id: source[:order_id]).first_or_initialize
+            order = Gemgento::Order.where(increment_id: source[:increment_id]).first_or_initialize
             order.order_id = source[:order_id]
             order.is_active = source[:is_active]
             order.user = User.where(magento_id: source[:customer_id]).first
