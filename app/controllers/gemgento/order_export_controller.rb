267b3553108c4eeaba82c1768eeaa475439caf6c
Kevin <kevin.pheasey@gmail.com>
2013-08-05 16:14:58 -0400

    Save order exports as a CSV

--- app/controllers/gemgento/order_export_controller.rb
+++ app/controllers/gemgento/order_export_controller.rb
@@ -6,41 +6,16 @@ module Gemgento
       raise 'No order item product attributes requested' if params[:order_item_product_attributes].nil?
       raise 'No order item attributes requested' if params[:order_item_attributes].nil?
 
-      @order_export = []
-      order_attributes = params[:order_attributes].split(',')
-      order_item_product_attributes = params[:order_item_product_attributes].split(',')
-      order_item_attributes = params[:order_item_attributes].split(',')
-      delimiter = params[:delimiter].nil? ? ',' : params[:delimiter]
-
-      Gemgento::Order.where('state != ?', 'cart').each do |order|
-        details = []
-
-        order.order_items.each do |order_item|
-          details = []
-
-          order_attributes.each do |attribute|
-            if Gemgento::Order.columns_hash[attribute].type != :datetime
-              details << order.send(attribute)
-            else
-              details << order.send(attribute).strftime("%Y%m%d%H%M%S")
-            end
-          end
-
-          order_item_product_attributes.each do |attribute|
-            details << order_item.product.attribute_value(attribute)
-          end
-
-          order_item_attributes.each do |attribute|
-            if Gemgento::OrderItem.columns_hash[attribute].type != :datetime
-              details << order_item.send(attribute)
-            else
-              details << order.send(attribute).strftime("%Y%m%d%H%M%S")
-            end
-          end
-        end
-
-        @order_export << details.join(delimiter)
-      end
+      @order_export = Gemgento::OrderExport.new(
+          params[:start_datetime].nil? ? (Time.now - 1.day).to_datetime : params[:start_datetime].to_datetime,
+          params[:order_attributes].split(','),
+          params[:order_item_product_attributes].split(','),
+          params[:order_item_attributes].split(','),
+          params[:delimiter].nil? ? ',' : params[:delimiter],
+          params[:path]
+      )
+      @order_export.generate
+      render @order_export.export_csv
     end
   end
 end
