7eda261bf62299e472e42c5c33d4fb9928b86848
Kevin <kevin.pheasey@gmail.com>
2013-08-21 10:12:26 -0400

    Use default devise controllers.  Override the methods in client applications.

--- app/controllers/gemgento/users/sessions_controller.rb
+++ app/controllers/gemgento/users/sessions_controller.rb
@@ -1,36 +1,5 @@
 module Gemgento
   class Users::SessionsController < Devise::SessionsController
 
-    ## GET /resource/sign_in
-    #def new
-    #  self.resource = resource_class.new(sign_in_params)
-    #  clean_up_passwords(resource)
-    #
-    #  respond_to do |format|
-    #    if user_signed_in?
-    #      format.html { render 'gemgento/users/info' }
-    #      format.js { render 'gemgento/users/sessions/successful_session', :layout => false }
-    #    else
-    #      format.html { render 'gemgento/users/sessions/new' }
-    #      format.js { render 'gemgento/users/sessions/errors', :layout => false }
-    #    end
-    #  end
-    #end
-    #
-    ## POST /resource/sign_in
-    #def create
-    #  user = User::is_valid_login(params[:user][:email], params[:user][:password])
-    #
-    #  respond_to do |format|
-    #    unless user.nil?
-    #      sign_in(:user, user)
-    #      format.html { render 'gemgento/users/info' }
-    #      format.js { render '/gemgento/users/sessions/successful_session', :layout => false }
-    #    else
-    #      format.html { render 'gemgento/users/sessions/new' }
-    #      format.js { render 'gemgento/users/sessions/errors', :layout => false }
-    #    end
-    #  end
-    #end
   end
 end
