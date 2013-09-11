c6ca4243c5e7f10b1fc9e054c4e7fb11f3fad61a
Kevin <kevin.pheasey@gmail.com>
2013-09-11 14:00:45 -0400

    Loading an attribute value loads the associated attribute.

--- app/models/gemgento/product_attribute_value.rb
+++ app/models/gemgento/product_attribute_value.rb
@@ -2,5 +2,7 @@ module Gemgento
   class ProductAttributeValue < ActiveRecord::Base
     belongs_to :product
     belongs_to :product_attribute
+
+    default_scope include: [:product_attribute]
   end
 end
