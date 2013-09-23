cbbbcc65b3116a5dc5a8a2d0d5c04b9b7282a91b
Kevin <kevin.pheasey@gmail.com>
2013-09-23 12:25:16 -0400

    Add checkout confirmation controller.

--- app/controllers/gemgento/checkout/confirmation_controller.rb
+++ app/controllers/gemgento/checkout/confirmation_controller.rb
@@ -1,5 +1,36 @@
 module Gemgento
   class Checkout::ConfirmationController < BaseController
 
+    def show
+      set_totals
+      @shipping_address = current_order.shipping_address
+      @billing_address = current_order.billing_address
+      @payment = current_order.order_payment
+      @cc_types = Gemgento::OrderPayment.cc_types
+      Rails.logger.info @cc_types
+
+      session[:shipping_methods].each do |shipping_method|
+        if shipping_method[:code] == current_order.shipping_method
+          @shipping_method = shipping_method
+          break
+        end
+      end
+    end
+
+    def update
+      current_order.enforce_cart_data
+
+      respond_to do |format|
+        if current_order.process
+          create_new_cart
+          format.html { redirect_to '/checkout/thank_you' }
+          format.js { render 'gemgento/checkout/confirm/success' }
+        else
+          format.html { redirect_to '/checkout/confirm' }
+          format.js { render 'gemgento/checkout/confirm/error' }
+        end
+      end
+    end
+
   end
 end
