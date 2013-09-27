ad1dfa51cfdcb921044df9a0aa83fcf84412c9d3
Kevin <kevin.pheasey@gmail.com>
2013-09-27 09:32:38 -0400

    Enable touch on the relation of product attribute values and assets to a product.

--- app/models/gemgento/order_item.rb
+++ app/models/gemgento/order_item.rb
@@ -1,7 +1,7 @@
 module Gemgento
   class OrderItem < ActiveRecord::Base
-    belongs_to  :order
-    belongs_to  :product
-    has_one     :gift_message
+    belongs_to :order, touch: true
+    belongs_to :product
+    has_one :gift_message
   end
 end
--- app/models/gemgento/product_attribute_value.rb
+++ app/models/gemgento/product_attribute_value.rb
@@ -1,6 +1,6 @@
 module Gemgento
   class ProductAttributeValue < ActiveRecord::Base
-    belongs_to :product
+    belongs_to :product, touch: true
     belongs_to :product_attribute
 
     default_scope -> { includes(:product_attribute) }
