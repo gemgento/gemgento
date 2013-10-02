33550554b8657c2b47b347d63600cc33a03905c7
Kevin <kevin.pheasey@gmail.com>
2013-10-02 11:54:53 -0400

    Use strong parameters for creating subscribers.

--- app/controllers/gemgento/subscribers_controller.rb
+++ app/controllers/gemgento/subscribers_controller.rb
@@ -2,14 +2,19 @@ module Gemgento
   class SubscribersController < ApplicationController
 
     def create
-      @subscriber = Subscriber.new
-      @subscriber.name = params[:subscriber][:name]
-      @subscriber.email = params[:subscriber][:email]
-      respond_to do |format|      
-        format.js { 
-          render :action => (@subscriber.save) ? 'create' : 'errors', :layout => false 
+      @subscriber = Gemgento::Subscriber.create(subscriber_params)
+
+      respond_to do |format|
+        format.js {
+          render :action => (@subscriber.save) ? 'create' : 'errors', :layout => false
         }
-      end    
+      end
+    end
+
+    private
+
+    def subscriber_params
+      params.require(:subscriber).permit(:name, :email)
     end
 
   end
