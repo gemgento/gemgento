c5d7491b74adab194720013415478179334d0220
Kevin <kevin.pheasey@gmail.com>
2013-10-09 16:50:55 -0400

    Add a ActiveRecord::Base method for creating and sanitizing custom SQL statements with variables

--- lib/gemgento/engine.rb
+++ lib/gemgento/engine.rb
@@ -12,6 +12,12 @@ module Gemgento
       if defined?(ActiveAdmin)
         ActiveAdmin.application.load_paths << File.dirname(__FILE__) + '/admin/'
       end
+
+      class ActiveRecord::Base
+        def self.escape_sql(clause, *rest)
+          self.send(:sanitize_sql_array, rest.empty? ? clause : ([clause] + rest))
+        end
+      end
     end
   end
 end
