cffa74f94019531c06ade4b35ebf219c914e74c8
Kevin <kevin.pheasey@gmail.com>
2013-10-14 16:38:34 -0400

    Fixed subscribers controller.

--- app/controllers/gemgento/subscribers_controller.rb
+++ app/controllers/gemgento/subscribers_controller.rb
@@ -1,13 +1,19 @@
 module Gemgento
-  class SubscribersController < ApplicationController
+  class SubscribersController < BaseController
+
+    def new
+      @subscriber = Subscriber.new
+    end
 
     def create
       @subscriber = Gemgento::Subscriber.create(subscriber_params)
 
       respond_to do |format|
-        format.js {
-          render :action => (@subscriber.save) ? 'create' : 'errors', :layout => false
-        }
+        if @subscriber.save
+          format.js { render action: 'create', layout: false }
+        else
+          format.js { render action: 'errors', layout: false }
+        end
       end
     end
 
