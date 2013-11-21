e280247e87d39a5e89a573a1e33ccd627d7b0a05
Kevin <kevin.pheasey@gmail.com>
2013-11-21 16:28:24 -0500

    Set ProductAttribute.scope from Magento.

--- app/controllers/gemgento/product_attributes_controller.rb
+++ app/controllers/gemgento/product_attributes_controller.rb
@@ -3,12 +3,13 @@ module Gemgento
 
     def update
       @product_attribute = Gemgento::ProductAttribute.find_or_initialize_by(magento_id: params[:id])
-      data = params[:id]
+      data = params[:data]
 
       @product_attribute.magento_id = data[:attribute_id]
       @product_attribute.code = data[:atribute_code]
       @product_attribute.frontend_input = data[:frontend_input]
       @product_attribute.default_value = data[:default_value]
+      @product_attribute.scope = data[:scope]
       @product_attribute.is_unique = data[:is_unique].to_i == 1 ? true : false
       @product_attribute.is_required = data[:is_required].to_i == 1 ? true : false
       @product_attribute.is_configurable = data[:is_configurable].to_i == 1 ? true : false
