aa208d915383092e4e464e641b88e7bf4026d5db
Kevin <kevin.pheasey@gmail.com>
2013-11-21 15:54:57 -0500

    Accept ProductAttributeSet updates from Magento

--- app/controllers/gemgento/categories_controller.rb
+++ app/controllers/gemgento/categories_controller.rb
@@ -20,7 +20,6 @@ module Gemgento
       @category.parent = Gemgento::Category.find_by(magento_id: params[:parent_id]) unless params[:parent_id].nil?
       @category.name = data[:name]
       @category.url_key = data[:url_key]
-
       @category.save
 
       render nothing: true
