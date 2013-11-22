22f7c376be55a65671d6a301ca408812946ee6f1
Kevin <kevin.pheasey@gmail.com>
2013-11-22 09:34:19 -0500

    Save ProductAttributeOptions from Magento push

--- app/controllers/gemgento/product_attributes_controller.rb
+++ app/controllers/gemgento/product_attributes_controller.rb
@@ -19,9 +19,28 @@ module Gemgento
       @product_attribute.is_used_for_promo_rules = data[:is_used_for_promo_rules].to_i == 1 ? true : false
       @product_attribute.is_visible_on_front = data[:is_visible_on_front].to_i == 1 ? true : false
       @product_attribute.used_in_product_listing = data[:used_in_product_listing].to_i == 1 ? true : false
+      @product_attribute.save
+
+      data[:options].each do |store_options|
+        @store = Gemgento::Store.find_by(magento_id: store_options[:store_id])
+
+        if @store
+          store_options[:options].each do |option|
+            @product_attribute_option = Gemgento::ProductAttributeOption.find_or_initialize_by(value: option[:value], store: @store)
+            @product_attribute_option.value = option[:value]
+            @product_attribute_option.label = option[:label]
+            @product_attribute_option.store = @store
+            @product_attribute_option.save
+          end
+        end
+      end
 
       render nothing: true
     end
 
-  end
-end
+    private
+
+    def self.
+
+        end
+    end
