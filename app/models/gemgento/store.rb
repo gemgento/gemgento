5957fcf3e88f6fb8ed884d181ed37955e6b3b1d6
Kevin <kevin.pheasey@gmail.com>
2013-12-13 11:21:50 -0500

    Add has_many relationships for orders, product_imports and product_attribute_options to stores

--- app/models/gemgento/store.rb
+++ app/models/gemgento/store.rb
@@ -1,5 +1,8 @@
 module Gemgento
   class Store < ActiveRecord::Base
+    has_many :orders
+    has_many :product_imports
+    has_many :product_attribute_options
     has_many :users
 
     has_and_belongs_to_many :products, -> { distinct }, join_table: 'gemgento_products_stores', class_name: 'Product'
