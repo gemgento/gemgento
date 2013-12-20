7588113afa034970a3c0f703fcbf7dea38add96b
Kevin <kevin.pheasey@gmail.com>
2013-12-20 13:57:32 -0500

    Put stores controller in gemgento namespace

--- app/controllers/gemgento/stores_controller.rb
+++ app/controllers/gemgento/stores_controller.rb
@@ -1,17 +1,19 @@
-class StoresController < BaseController
-  def update
-    data = params[:data]
+module Gemgento
+  class StoresController < BaseController
+    def update
+      data = params[:data]
 
-    @store = Gemgento::Store.find_or_initialize_by(magento_id: data[:store_id])
-    @store.magento_id = source[:store_id]
-    @store.code = source[:code]
-    @store.website_id = source[:website_id]
-    @store.group_id = source[:group_id]
-    @store.name = source[:name]
-    @store.sort_order = source[:sort_order]
-    @store.is_active = source[:is_active]
-    @store.save
+      @store = Gemgento::Store.find_or_initialize_by(magento_id: data[:store_id])
+      @store.magento_id = source[:store_id]
+      @store.code = source[:code]
+      @store.website_id = source[:website_id]
+      @store.group_id = source[:group_id]
+      @store.name = source[:name]
+      @store.sort_order = source[:sort_order]
+      @store.is_active = source[:is_active]
+      @store.save
 
-    render nothing: true
+      render nothing: true
+    end
   end
 end
