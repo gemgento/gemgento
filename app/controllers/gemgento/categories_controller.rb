4fd7ac5a8f1488ca72506b25b5e2e1016a9ba215
Kevin <kevin.pheasey@gmail.com>
2014-01-15 11:35:25 -0500

    Category#Show and Category#Index accept an updated_at filter.

--- app/controllers/gemgento/categories_controller.rb
+++ app/controllers/gemgento/categories_controller.rb
@@ -4,14 +4,23 @@ module Gemgento
     respond_to :json, :html
 
     def index
-      @categories = Gemgento::Category.where(include_in_menu: true)
+      if params[:updated_at] # only grab categories that were updated after specified timestamp
+        @categories = Gemgento::Category.where('updated_at > ?', params[:updated_at]).where(include_in_menu: true)
+      else
+        @categories = Gemgento::Category.where(include_in_menu: true)
+      end
 
       respond_with @categories
     end
 
     def show
-      @category = Gemgento::Category.find(params[:id])
-      @category.includes_category_products = true
+      if params[:updated_at] # only grab the category if it was updated after specified timestamp
+        @category = Gemgento::Category.where('updated_at > ? AND id = ?', params[:updated_at], params[:id]).first
+      else
+        @category = Gemgento::Category.find(params[:id])
+      end
+
+      @category.includes_category_products = true unless @category.nil?
 
       respond_with @category
     end
