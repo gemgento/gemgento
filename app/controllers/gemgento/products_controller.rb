276a19f3d539e7f76b9486e3f634f67e9e379b16
Kevin <kevin.pheasey@gmail.com>
2014-01-17 11:55:05 -0500

    Magento now pushes simple and configurable product associations on Product#Update

--- app/controllers/gemgento/products_controller.rb
+++ app/controllers/gemgento/products_controller.rb
@@ -48,6 +48,8 @@ module Gemgento
         set_attribute_values_from_magento(data[:additional_attributes], @product)
       end
 
+      set_associated_products(data[:simple_product_ids], data[:configurable_product_ids], @product)
+
       render nothing: true
     end
 
@@ -151,6 +153,39 @@ module Gemgento
           end
         end
       end
+    end
+
+    def set_associated_products(simple_magento_product_ids, configurable_magento_product_ids, product)
+      set_simple_products(simple_magento_product_ids, product) unless simple_magento_product_ids.nil?
+      set_configurable_products(configurable_magento_product_ids, product) unless configurable_magento_product_ids.nil?
+    end
+
+    def set_simple_products(magento_ids, product)
+      simple_product_ids = []
+
+      magento_ids.each do |magento_id|
+        simple_product = Gemgento::Product.find_by(magento_id: magento_id)
+        next if product.nil?
+
+        product.simple_products << simple_product unless product.simple_products.include? simple_product
+        simple_product_ids << simple_product.id
+      end
+
+      product.simple_products.where('simple_product_id NOT IN (?)', simple_product_ids).clear
+    end
+
+    def set_configurable_products(magento_ids, product)
+      configurable_product_ids = []
+
+      magento_ids.each do |magento_id|
+        configurable_product = Gemgento::Product.find_by(magento_id: magento_id)
+        next if product.nil?
+
+        product.configurable_products << configurable_product unless product.configurable_products.include? configurable_product
+        configurable_product_ids << configurable_product.id
+      end
+
+      product.configurable_products.where('configurable_product_id NOT IN (?)', configurable_product_ids).clear
     end
 
   end
