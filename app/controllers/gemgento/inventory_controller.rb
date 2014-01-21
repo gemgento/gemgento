a9ecbbf98522f1494aecf0eeed144b84d98a8197
Kevin <kevin.pheasey@gmail.com>
2014-01-21 13:31:48 +0000

    Don't accept pushed data if the store or website doesn't exist.

--- app/controllers/gemgento/inventory_controller.rb
+++ app/controllers/gemgento/inventory_controller.rb
@@ -7,14 +7,16 @@ module Gemgento
       product = Gemgento::Product.find_by(magento_id: data[:product_id])
 
       unless data[:inventories].nil?
-        data[:inventories].each do |store_id, stock_data|
-          store = Gemgento::Store.find_by(magento_id: store_id)
-          inventory = Gemgento::Inventory.find_or_initialize_by(store: store, product: product)
+        data[:inventories].each do |website_id, stock_data|
+          store = Gemgento::Store.find_by(website_id: website_id)
+          next if store.nil?
 
+          inventory = Gemgento::Inventory.find_or_initialize_by(store: store, product: product)
           inventory.product = product
           inventory.store = store
           inventory.quantity = stock_data[:qty]
           inventory.is_in_stock = stock_data[:is_in_stock]
+          inventory.use_default_website_stock = stock_data[:use_default_website_stock].nil? ? true : stock_data[:use_default_website_stock]
           inventory.sync_needed = false
           inventory.save
         end
