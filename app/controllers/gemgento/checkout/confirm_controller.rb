2ef703bc1aeb7639173abbfde04f86f53f7473f1
Kevin <kevin.pheasey@gmail.com>
2014-01-22 15:41:53 +0000

    Add JSON support to Checkout::ConfirmController

--- app/controllers/gemgento/checkout/confirm_controller.rb
+++ app/controllers/gemgento/checkout/confirm_controller.rb
@@ -3,6 +3,8 @@ module Gemgento
     before_filter :auth_cart_contents
     before_filter :auth_order_user
 
+    respond_to :json, :html
+
     def show
       set_totals
       @shipping_address = current_order.shipping_address
@@ -16,19 +18,45 @@ module Gemgento
           break
         end
       end
+
+      respond_to do |format|
+        format.html
+        format.json do
+          render json: {
+              order: current_order,
+              cc_types: Gemgento::OrderPayment.cc_types,
+              total: @total,
+              tax: @tax,
+              shipping: @shipping
+          }
+        end
+      end
+
     end
 
     def update
       current_order.enforce_cart_data
       @order = current_order
 
-      if current_order.process
-        @order.reload
-        redirect_to checkout_thank_you_path
-      else
-        flash[:error] = 'There was a problem processing your order.  Please review order details and try again.'
-        redirect_to checkout_confirm_path
+      respond_to do |format|
+        if current_order.process
+          @order.reload
+
+          format.html { redirect_to checkout_thank_you_path }
+          format.json { render json: { result: true, order: @order } }
+        else
+          flash[:error] =
+
+          format.html { redirect_to checkout_confirm_path }
+          format.json do
+            render json: {
+                result: false,
+                errors: 'There was a problem processing your order.  Please review order details and try again.'
+            }
+          end
+        end
       end
+
     end
 
   end
