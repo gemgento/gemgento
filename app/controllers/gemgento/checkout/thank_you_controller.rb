453d63c760dd40065f6bfe6fc96ff7f70e477b43
Kevin <kevin.pheasey@gmail.com>
2014-01-22 15:49:18 +0000

    Add JSON support to Checkout::ThankYouController

--- app/controllers/gemgento/checkout/thank_you_controller.rb
+++ app/controllers/gemgento/checkout/thank_you_controller.rb
@@ -1,9 +1,13 @@
 module Gemgento
   class Checkout::ThankYouController < Checkout::CheckoutBaseController
 
+    respond_to :json, :html
+
     def show
       @order = current_order
       create_new_cart
+
+      respond_with @order
     end
 
   end
