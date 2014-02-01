6f30671692931b7abe6ea0fc4cb10d75f99b56da
Kevin <kevin.pheasey@gmail.com>
2014-02-01 13:06:03 -0500

    Add JSON response to User::SessionsController#Crate

--- app/controllers/gemgento/users/sessions_controller.rb
+++ app/controllers/gemgento/users/sessions_controller.rb
@@ -1,8 +1,22 @@
 module Gemgento
   class Users::SessionsController < Devise::SessionsController
     include SslRequirement
-
     ssl_required :new, :create, :destroy
+    respond_to :html, :json
+
+    # POST /resource/sign_in
+    def create
+      self.resource = warden.authenticate!(auth_options)
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+
+      respond_to do |format|
+        format.html { respond_with resource, :location => after_sign_in_path_for(resource) }
+        format.json { render json: { result: true, user: resource } }
+      end
+
+    end
 
     # DELETE /resource/sign_out
     def destroy
