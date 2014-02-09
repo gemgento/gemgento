0411744e8b0fc09db4cf18af245cfe9a5ff4195c
Kevin <kevin.pheasey@gmail.com>
2014-02-08 21:05:22 -0500

    Fix shipment api call responses

--- app/models/gemgento/api/soap/sales/order_shipment.rb
+++ app/models/gemgento/api/soap/sales/order_shipment.rb
@@ -4,7 +4,7 @@ module Gemgento
       module Sales
         class OrderShipment
 
-          def add_comment(shipment_comment, email = nil, include_in_email = nil)
+          def self.add_comment(shipment_comment, email = nil, include_in_email = nil)
             message = {
                 shipment_increment_id: shipment_comment.shipment.increment_id,
                 comment: shipment_comment.comment,
@@ -16,7 +16,7 @@ module Gemgento
             return response.success?
           end
 
-          def add_track(shipment_track)
+          def self.add_track(shipment_track)
             message = {
                 shipment_increment_id: shipment_track.shipment.increment_id,
                 carrier: shipment_track.carrier,
@@ -28,7 +28,7 @@ module Gemgento
             return response.success?
           end
 
-          def create(order_increment_id, email = nil, comment = nil, include_comment = nil)
+          def self.create(order_increment_id, email = nil, comment = nil, include_comment = nil)
             message = {
                 order_increment_id: order_increment_id,
                 email: email,
@@ -38,52 +38,53 @@ module Gemgento
             response = Gemgento::Magento.create_call(:sales_order_shipment_create, message)
 
             if response.success?
-              shipment.increment_id = response['shipmentIncrementId']
-              shipment.save
-
-              return true
+              return response.body[:shipment_increment_id]
             else
               return false
             end
           end
 
-          def get_carriers(order_increment_id)
+          def self.get_carriers(order_increment_id)
             message = {
                 order_increment_id: order_increment_id,
             }
             response = Gemgento::Magento.create_call(:sales_order_shipment_get_carriers, message)
 
             if response.success?
-              return response[:result]
+              if response.body[:result][:item].nil?
+                return response.body[:result]
+              else
+                response.body[:result][:item]
+              end
             else
-              return nil
+              return false
             end
           end
 
-          def info(shipment_increment_id)
+          def self.info(shipment_increment_id)
             message = {
                 shipment_increment_id: shipment_increment_id,
             }
             response = Gemgento::Magento.create_call(:sales_order_shipment_info, message)
 
             if response.success?
-              return response[:result]
+              return response.body[:result]
             else
               return nil
             end
           end
 
-          def list
+          def self.list
             response = Gemgento::Magento.create_call(:sales_order_shipment_list)
 
             if response.success?
-              return response[:result]
+              return response.body[:result]
             else
-              return nil
+              return false
             end
           end
 
-          def remove_track(shipment_increment_id, track_id)
+          def self.remove_track(shipment_increment_id, track_id)
             message = {
                 shipment_increment_id: shipment_increment_id,
                 track_id: track_id
