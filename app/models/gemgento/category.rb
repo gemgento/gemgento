bbcd8efb2efe60a69ad5c3743418a5ede9fbcfe7
Kevin <kevin.pheasey@gmail.com>
2014-02-18 14:58:35 -0500

    Fixed the Category.top_level scope

--- app/models/gemgento/category.rb
+++ app/models/gemgento/category.rb
@@ -14,7 +14,7 @@ module Gemgento
 
     default_scope -> { order(:position) }
 
-    scope :top_level, -> { where(:parent_id => 2) }
+    scope :top_level, -> { where(parent: Gemgento::Category.find_by(parent_id: nil)) }
 
     def save
       # Dirty dirty dirty(S3Bug)..
