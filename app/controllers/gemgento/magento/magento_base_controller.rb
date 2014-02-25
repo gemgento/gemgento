c9dfc2a9ccfe0acd8cd62f9affac138b1cf01c88
Kevin <kevin.pheasey@gmail.com>
2014-02-25 16:27:42 -0500

    Convert ip white list to string so it can be split

--- app/controllers/gemgento/magento/magento_base_controller.rb
+++ app/controllers/gemgento/magento/magento_base_controller.rb
@@ -3,10 +3,7 @@ module Gemgento
     before_filter :validate_ip
 
     def validate_ip
-      whitelist = Gemgento::Config[:magento][:ip_whitelist].split(',')
-
-      puts request.remote_ip.inspect
-      puts whitelist.inspect
+      whitelist = Gemgento::Config[:magento][:ip_whitelist].to_s.split(',')
 
       if Rails.env.production? && !whitelist.include?(request.remote_ip)
         raise ActionController::RoutingError.new('Not Found')
