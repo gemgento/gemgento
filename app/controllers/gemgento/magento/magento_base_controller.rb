69f926784cf74c2f6c92cd000905dac140e94ad8
Kevin <kevin.pheasey@gmail.com>
2014-02-25 14:02:29 -0500

    Add default Gemgento configuration settings.

--- app/controllers/gemgento/magento/magento_base_controller.rb
+++ app/controllers/gemgento/magento/magento_base_controller.rb
@@ -1,5 +1,14 @@
 module Gemgento
   class Magento::MagentoBaseController < BaseController
+    before_filter :validate_ip
+
+    def validate_ip
+      whitelist = Gemgento::Config[:magento][:ip_whitelist].split(',')
+
+      if Rails.env.production? && !whitelist.include?(require.remote_ip)
+        raise ActionController::RoutingError.new('Not Found')
+      end
+    end
 
   end
 end
--- config/initializers/gemgento_config.rb
+++ config/initializers/gemgento_config.rb
@@ -1,5 +1,24 @@
 yaml = YAML.load(File.read(Rails.root.join('config', 'gemgento_config.yml')))
-Gemgento::Config = HashWithIndifferentAccess.new(yaml[Rails.env])
+
+# configuration settings
+config_defaults = {
+  magento: {
+    url: 'www.gemgento.com',
+    auth_username: '',
+    auth_password: '',
+    username:         'gemgento',
+    api_key:          '',
+    api_type:         'soap',
+    api_version:      '2',
+    encryption:       '',
+    debug:            false,
+    root:             '/var/www/magento/public',
+    ip_whitelist:     '127.0.0.01'
+  },
+  sellect: false
+}
+
+Gemgento::Config = HashWithIndifferentAccess.new(yaml[Rails.env]).reverse_merge config_defaults
 
 # Don't open files as StringIO
 OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
