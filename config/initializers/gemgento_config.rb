5d67dc8666f3c0c1bbc585f809fd285d3eaac6f4
Kevin <kevin.pheasey@gmail.com>
2014-04-04 15:53:42 -0400

    Add gemgento:install generator.

--- config/initializers/gemgento_config.rb
+++ config/initializers/gemgento_config.rb
@@ -1,23 +1,59 @@
+# create a default configuration file if none exists
+unless File.exist?(Rails.root.join('config', 'gemgento_config.yml'))
+  File.open(Rails.root.join('config', 'gemgento_config.yml'), 'w') do |f|
+    f.write({
+                development: {
+                    magento: {
+                        url: 'www.gemgento.com',
+                        username:         'gemgento',
+                        api_key:          '',
+                        encryption:       '',
+                        debug:            false,
+                    },
+                },
+                beta: {
+                    magento: {
+                        url: 'www.gemgento.com',
+                        username:         'gemgento',
+                        api_key:          '',
+                        encryption:       '',
+                    },
+                },
+                production: {
+                    magento: {
+                        url: 'www.gemgento.com',
+                        username:         'gemgento',
+                        api_key:          '',
+                        encryption:       '',
+                        ip_whitelist:     '127.0.0.1'
+                    },
+                }
+            }.to_yaml)
+  end
+end
+
+# load the configuation file
 yaml = YAML.load(File.read(Rails.root.join('config', 'gemgento_config.yml')))
 
 # configuration settings
 config_defaults = {
-  magento: {
-    url: 'www.gemgento.com',
-    auth_username: '',
-    auth_password: '',
-    username:         'gemgento',
-    api_key:          '',
-    api_type:         'soap',
-    api_version:      '2',
-    encryption:       '',
-    debug:            false,
-    root:             '/var/www/magento/public',
-    ip_whitelist:     '127.0.0.1'
-  },
-  sellect: false
+    magento: {
+        url: 'www.gemgento.com',
+        auth_username: '',
+        auth_password: '',
+        username:         'gemgento',
+        api_key:          '',
+        api_type:         'soap',
+        api_version:      '2',
+        encryption:       '',
+        debug:            false,
+        root:             '/var/www/magento/public',
+        ip_whitelist:     '127.0.0.1'
+    },
+    sellect: false
 }
 
+# create the configuration hash from environment specifics in configuration file
 Gemgento::Config = HashWithIndifferentAccess.new(config_defaults).deep_merge HashWithIndifferentAccess.new(yaml[Rails.env])
 
 # Don't open files as StringIO
---
+++ lib/generators/gemgento/install_generator.rb
@@ -0,0 +1,24 @@
+module Gemgento
+  class InstallGenerator < Rails::Generators::Base
+
+    desc 'Copies migrations to your application.'
+    def copy_migrations
+      rake('gemgento:install:migrations')
+    end
+
+    desc 'Add the Gemgento routes'
+    def add_routes
+      route "mount Gemgento::Engine, :at => '/'"
+    end
+
+    desc 'Include the Gemgento::ApplicationHelper'
+    def include_application_helper
+      inject_into_file Rails.root.join('app', 'controllers', 'application_controller.rb'), before: 'class ApplicationController' do <<-'RUBY'
+include Gemgento::ApplicationHelper
+
+      RUBY
+      end
+    end
+
+  end
+end
