137a5c1b7290a1264ee41d3ea7ae8f88edc09c37
Kevin <kevin.pheasey@gmail.com>
2014-04-18 14:34:36 -0400

    Testing new layout

---
+++ app/models/gemgento/magento.rb
@@ -0,0 +1,87 @@
+module Gemgento
+  class Magento
+    # Log into the Magento API and setup the session and client
+    def self.api_login(force_new_session = false)
+      @api_url = "http://#{Gemgento::Config[:magento][:url]}/index.php/api/v#{Gemgento::Config[:magento][:api_version]}_#{Gemgento::Config[:magento][:api_type]}/index/wsdl/1"
+      @client = Savon.client(
+          wsdl: @api_url,
+          log: Gemgento::Config[:magento][:debug],
+          raise_errors: false,
+          basic_auth: [Gemgento::Config[:magento][:auth_username].to_s, Gemgento::Config[:magento][:auth_password].to_s],
+          open_timeout: 300,
+          read_timeout: 300
+      )
+
+      @session = Gemgento::Session.get(@client, force_new_session)
+    end
+
+    # Make an API call to Magento and get the response
+    #
+    # @param [Symbol] function  The API call to make
+    # @param [Hash]   message   Call parameters (does not need session)
+    # @return [Hash]
+    def self.create_call(function, message = {})
+      api_login if !defined? @client
+
+      message[:sessionId] = @session
+      Rails.logger.debug "Making Call - #{function}"
+
+      magento_response = MagentoResponse.new
+
+      # don't save some messages because they are just too big!
+      if [:catalog_product_attribute_media_create].include? function
+        magento_response.request = {function: function, message: ''}
+      else
+        magento_response.request = {function: function, message: message}
+      end
+
+      response = @client.call(function, message: message)
+
+      if response.success?
+        magento_response.success = true
+
+        if [:customer_customer_list, :sales_order_list, :catalog_product_list].include? function
+          magento_response.body = response.body[:"body_too_big"]
+          magento_response.body_overflow = response.body[:"#{function}_response"]
+        else
+          magento_response.body = response.body[:"#{function}_response"]
+        end
+
+        # only save successful responses if debugging is enabled
+        magento_response.save if Gemgento::Config[:magento][:debug]
+
+        Rails.logger.debug '^^^ Success ^^^'
+      else
+        magento_response.success = false
+        magento_response.body = response.body[:fault]
+        Rails.logger.warn '^^^ Failure ^^^'
+
+        if !magento_response.body[:faultcode].nil? && magento_response.body[:faultcode].to_i == 5
+          Rails.logger.debug '--- Attempting To Start New Session ---'
+          api_login(true)
+          create_call(function, message)
+        end
+
+        magento_response.save
+      end
+
+      return magento_response
+    end
+
+    def self.enforce_savon_string(subject)
+      if subject.is_a? String
+        subject
+      else
+        ''
+      end
+    end
+
+    def self.enforce_savon_array(subject)
+      if subject.is_a? Array
+        subject
+      else
+        [subject]
+      end
+    end
+  end
+end
--- lib/gemgento.rb
+++ lib/gemgento.rb
@@ -10,91 +10,64 @@ require 'gemgento/version'
 require 'gemgento/engine'
 
 module Gemgento
-  class Magento
-
-    # Log into the Magento API and setup the session and client
-    def self.api_login(force_new_session = false)
-      @api_url = "http://#{Gemgento::Config[:magento][:url]}/index.php/api/v#{Gemgento::Config[:magento][:api_version]}_#{Gemgento::Config[:magento][:api_type]}/index/wsdl/1"
-      @client = Savon.client(
-          wsdl: @api_url,
-          log: Gemgento::Config[:magento][:debug],
-          raise_errors: false,
-          basic_auth: [Gemgento::Config[:magento][:auth_username].to_s, Gemgento::Config[:magento][:auth_password].to_s],
-          open_timeout: 300,
-          read_timeout: 300
-      )
-
-      @session = Gemgento::Session.get(@client, force_new_session)
+  module SslRequirement
+    def self.included(controller)
+      controller.extend(ClassMethods)
+      controller.before_filter(:ensure_proper_protocol)
     end
 
-    # Make an API call to Magento and get the response
-    #
-    # @param [Symbol] function  The API call to make
-    # @param [Hash]   message   Call parameters (does not need session)
-    # @return [Hash]
-    def self.create_call(function, message = {})
-      api_login if !defined? @client
-
-      message[:sessionId] = @session
-      Rails.logger.debug "Making Call - #{function}"
-
-      magento_response = MagentoResponse.new
-
-      # don't save some messages because they are just too big!
-      if [:catalog_product_attribute_media_create].include? function
-        magento_response.request = {function: function, message: ''}
-      else
-        magento_response.request = {function: function, message: message}
+    module ClassMethods
+      # Specifies that the named actions requires an SSL connection to be performed (which is enforced by ensure_proper_protocol).
+      def ssl_required(*actions)
+        class_attribute(:ssl_required_actions)
+        self.ssl_required_actions = actions
       end
 
-      response = @client.call(function, message: message)
-
-      if response.success?
-        magento_response.success = true
-
-        if [:customer_customer_list, :sales_order_list, :catalog_product_list].include? function
-          magento_response.body = response.body[:"body_too_big"]
-          magento_response.body_overflow = response.body[:"#{function}_response"]
-        else
-          magento_response.body = response.body[:"#{function}_response"]
-        end
-
-        # only save successful responses if debugging is enabled
-        magento_response.save if Gemgento::Config[:magento][:debug]
-
-        Rails.logger.debug '^^^ Success ^^^'
-      else
-        magento_response.success = false
-        magento_response.body = response.body[:fault]
-        Rails.logger.warn '^^^ Failure ^^^'
-
-        if !magento_response.body[:faultcode].nil? && magento_response.body[:faultcode].to_i == 5
-          Rails.logger.debug '--- Attempting To Start New Session ---'
-          api_login(true)
-          create_call(function, message)
-        end
-
-        magento_response.save
-      end
-
-      return magento_response
-    end
-
-    def self.enforce_savon_string(subject)
-      if subject.is_a? String
-        subject
-      else
-        ''
+      def ssl_allowed(*actions)
+        class_attribute(:ssl_allowed_actions)
+        self.ssl_allowed_actions = actions
       end
     end
 
-    def self.enforce_savon_array(subject)
-      if subject.is_a? Array
-        subject
+    protected
+    # Returns true if the current action is supposed to run as SSL
+    def ssl_required?
+      if self.class.respond_to?(:ssl_required_actions)
+        actions = self.class.ssl_required_actions
+        actions.empty? || actions.include?(action_name.to_sym)
       else
-        [subject]
+        return false
+      end
+    end
+
+    def ssl_allowed?
+      if self.class.respond_to?(:ssl_allowed_actions)
+        actions = self.class.ssl_allowed_actions
+        actions.empty? || actions.include?(action_name.to_sym)
+      else
+        return false
+      end
+    end
+
+    private
+
+    def ssl_supported?
+      if Gemgento::Config[:require_ssl] == false
+        return false
+      else
+        return Rails.env.production?
+      end
+    end
+
+    def ensure_proper_protocol
+      return true if ssl_allowed?
+      if ssl_required? && !request.ssl? && ssl_supported?
+        redirect_to "https://" + request.host + request.fullpath
+        flash.keep
+      elsif request.ssl? && !ssl_required?
+        redirect_to "http://" + request.host + request.fullpath
+        flash.keep
       end
     end
   end
-
 end
