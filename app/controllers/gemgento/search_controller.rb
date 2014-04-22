c36e2797b832c1deca2d5441bd6bc5a652742cb8
C.J. Bordeleau <bordeleau.cj@gmail.com>
2014-04-22 15:19:18 -0400

    Add json support to search controller.

--- app/controllers/gemgento/search_controller.rb
+++ app/controllers/gemgento/search_controller.rb
@@ -1,8 +1,15 @@
 module Gemgento
   class SearchController < ApplicationController
 
+    respond_to :json, :html
+
     def index
       @results = Gemgento::Search.products(params[:query])
+
+      respond_to do |format|
+        format.html
+        format.json {render json: @results}
+      end
     end
 
   end
