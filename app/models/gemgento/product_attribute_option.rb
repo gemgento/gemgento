af0f9215f83562f8a451a775b764053359aa2a1a
Kevin <kevin.pheasey@gmail.com>
2014-04-30 10:35:13 -0400

    Use a lambda in place of scope for the ProductAttributeOption / ProductAttributeValue association.

--- app/models/gemgento/product_attribute_option.rb
+++ app/models/gemgento/product_attribute_option.rb
@@ -4,15 +4,15 @@ module Gemgento
     belongs_to :store
 
     has_many :product_attribute_values,
-             foreign_key: 'value',
-             primary_key: 'value',
-             conditions: Proc.new { |join_association|
-               if join_association
-                 'gemgento_product_attribute_values.product_attribute_id = gemgento_product_attribute_options.product_attribute_id'
+              ->(join_or_model) {
+               if join_or_model.is_a? JoinDependency::JoinAssociation
+                 where('gemgento_product_attribute_values.product_attribute_id = gemgento_product_attribute_options.product_attribute_id')
                else
-                 { product_attribute_id: product_attribute_id }
+                 where(product_attribute_id: join_or_model.product_attribute_id)
                end
-             }
+             },
+             foreign_key: 'value',
+             primary_key: 'value'
     has_many :products, through: :product_attribute_values
 
     default_scope -> { order(:order) }
--- app/models/gemgento/product_attribute_value.rb
+++ app/models/gemgento/product_attribute_value.rb
@@ -4,15 +4,15 @@ module Gemgento
     belongs_to :product_attribute
     belongs_to :store
     belongs_to :product_attribute_option,
-               foreign_key: 'value',
-               primary_key: 'value',
-               conditions: Proc.new { |join_association|
-                 if join_association
-                   'gemgento_product_attribute_options.product_attribute_id = gemgento_product_attribute_values.product_attribute_id'
+               ->(join_or_model) {
+                 if join_or_model.is_a? JoinDependency::JoinAssociation
+                   where('gemgento_product_attribute_options.product_attribute_id = gemgento_product_attribute_values.product_attribute_id')
                  else
-                   { product_attribute_id: product_attribute_id }
+                   where(product_attribute_id: join_or_model.product_attribute_id)
                  end
-               }
+               },
+               foreign_key: 'value',
+               primary_key: 'value'
 
     default_scope -> { includes(:product_attribute) }
 
