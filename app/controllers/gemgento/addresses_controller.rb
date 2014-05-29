ed885b4800868b1e1336ec8630e2a37a613a039b
Kevin <kevin.pheasey@gmail.com>
2014-05-29 08:56:11 -0400

    AddressController#region_options now responds to JSON

--- app/controllers/gemgento/addresses_controller.rb
+++ app/controllers/gemgento/addresses_controller.rb
@@ -2,11 +2,15 @@ module Gemgento
   class AddressesController < Gemgento::ApplicationController
     ssl_allowed :region_options
 
+    respond_to :html, :json
+
     layout -> { set_layout false }
 
     def region_options
       country = Country.find(params[:country_id])
       @regions = Region.where(country: country).map { |r| [r.name, r.id] }
+
+      respond_with @regions
     end
   end
 end
