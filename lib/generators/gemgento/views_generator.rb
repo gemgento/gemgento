bb4b04d723f481bea6dbfc4afbee0b2d0a8ac4d0
Kevin <kevin.pheasey@gmail.com>
2014-06-03 16:45:43 -0400

    Views generator no installs the layout files.  Add assets generator to install javascript and stylesheets.

--- lib/generators/gemgento/views_generator.rb
+++ lib/generators/gemgento/views_generator.rb
@@ -3,7 +3,7 @@ module Gemgento
 
     desc 'Copies migrations to your application.'
     def copy_views
-      FileUtils.cp_r Gemgento::Engine.root.join('app', 'views', 'gemgento'), Rails.application.root.join('app', 'views')
+      FileUtils.cp_r Gemgento::Engine.root.join('app', 'views'), Rails.application.root.join('app')
     end
 
   end
