a9ac5cf39cf3ed2b5e2dcb072ac7815bb928f72e
Kevin <kevin.pheasey@gmail.com>
2014-06-11 14:57:39 -0400

    Update checkout gift controller to redirect when form is submitted

--- app/controllers/gemgento/checkout/gift_controller.rb
+++ app/controllers/gemgento/checkout/gift_controller.rb
@@ -4,13 +4,10 @@ module Gemgento
 
     def update
       current_order.gift_message = params[:order][:gift_message]
-      # @order.status.sync_needed = false
       current_order.save
 
-      puts "current_order.gift_message ====>>>> #{current_order.gift_message}"
-
       respond_to do |format|
-        format.html { render '/checkout/signin' }        
+        format.html { redirect_to checkout_login_path }
         format.json { render json: { result: true, order: current_order } }
       end
     end
