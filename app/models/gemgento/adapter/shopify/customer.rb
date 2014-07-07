3c9c0ee48273127f58c2a30c03ff028621f1e3c2
Kevin <kevin.pheasey@gmail.com>
2014-07-07 16:33:11 -0400

    Import customers from Shopify to Gemgento.

--- app/models/gemgento/adapter/shopify/customer.rb
+++ app/models/gemgento/adapter/shopify/customer.rb
@@ -3,20 +3,31 @@ require 'shopify_api'
 module Gemgento::Adapter::Shopify
   class Customer
 
+    # Import all customers from Shopify.
+    #
+    # @return [Void]
     def self.import
       ShopifyAPI::Base.site = Gemgento::Adapter::ShopifyAdapter.api_url
 
       ShopifyAPI::Customer.all.each do |customer|
-        user = create_user(customer)
+        create_user(customer)
       end
     end
 
     # Create a user from Shopify customer.
     #
-    # @param customer [ShopifyAPI::Customer]
+    # @param shopify_customer [ShopifyAPI::Customer]
     # @return [Gemgento::User]
-    def create_user(customer)
-      user = Gemgento::User.find_or_initialize_by(email: customer.email)
+    def create_user(shopify_customer)
+      user = Gemgento::User.find_or_initialize_by(email: shopify_customer.email)
+      user.first_name = shopify_customer.first_name
+      user.last_name = shopify_customer.last_name
+      user.user_group = Gemgento::UserGroup.find_by(code: 'General')
+      user.sync_needed = true
+      user.save
+
+      Gemgento::Adapter::ShopifyAdapter.create_association(user, shopify_customer)
+
       return user
     end
 
