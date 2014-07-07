86ad62b6ec9de2364fcc1376d34f9d23f03bcadb
Kevin <kevin.pheasey@gmail.com>
2014-07-07 16:08:44 -0400

    Import Shopify addresses

--- app/models/gemgento/adapter/shopify/address.rb
+++ app/models/gemgento/adapter/shopify/address.rb
@@ -6,9 +6,30 @@ module Gemgento::Adapter::Shopify
     # @param address [ShopifyAPI::Address]
     # @param user [Gemgento::User]
     # @param is_default [Boolean]
-    def self.import(address, user, is_default)
-      address = Gemgento::Address.new
+    def self.import(shopify_address, user)
+      if adapter_model = Gemgento::Adapter::ShopifyAdapter.find_by_shopify_model(shopify_address)
+        address = adapter_model.gemgento_model
+      else
+        address = Gemgento::Address.new
+      end
 
+      address.user = user
+      address.address1 = shopify_address.address1
+      address.address2 = shopify_address.address2
+      address.city = shopify_address.city
+      address.company = shopify_address.company
+      address.country = Gemgento::Country.find_by(iso2_code: shopify_address.country_code)
+      address.first_name = shopify_address.first_name
+      address.last_name = shopify_address.last_name
+      address.telephone = shopify_address.phone
+      address.region = Gemgento::Region.find_by(code: shopify_address.province_code)
+      address.postcode = shopify_address.zip
+      address.is_default_billing = shopify_address.default ? true : false
+      address.is_default_shipping = shopify_address.default ? true : false
+      address.sync_needed = true
+      address.save
+
+      Gemgento::Adapter::ShopifyAdapter.create_association(address, shopify_address) if address.shopify_adapter.empty?
     end
 
   end
