a24cf550bb67a88df68086eada2a9b9fba4c2539
Kevin <kevin.pheasey@gmail.com>
2014-07-14 11:14:27 -0400

    Mapped all the order attributes for export.  Need to fill in some sub methods.

--- app/models/gemgento/adapter/shopify/order.rb
+++ app/models/gemgento/adapter/shopify/order.rb
@@ -56,73 +56,73 @@ module Gemgento::Adapter::Shopify
     # @return [Array(String)]
     def self.order_row(order)
       user = Gemgento::User.find_by(email: order.email)
-      store = store(order.zone_member_id)
-      shipping_addres = address(order.ship_address_id)
-      billing_address = address(order.bill_address_id)
-      payment = payment(order.id)
-      totals = totals(order, payment)
-      line_items = line_items(order.id).reload
+      store = store(order)
+      shipping_address = order.shipping_address
+      billing_address = order.billing_address
+      payment = order.payment_details
+      totals = totals(order)
+      line_items = order.line_items
 
       [
-          order.number.gsub('R', ''), # order_id
+          order.order_number, # order_id
           store.code, # website
-          email(order.email), # email
+          order.email, # email
           '', # password
           user_group(user), # group_id
           store.magento_id, # store_id
-          'globalcollect_cc_merchant', # payment_method
+          'payment_cryozonic_stripe', # payment_method
           'flatrate_flatrate', # shipping_method
           '', # billing_prefix
-          billing_address.firstname, # billing_firstname
+          billing_address.first_name, # billing_firstname
           '', # billing_middlename
-          billing_address.lastname, # billing_lastname
+          billing_address.last_name, # billing_lastname
           '', # billing_suffix
           street(billing_address), # billing_street_full
           billing_address.city, # billing_city
-          state(billing_address), # billing_region
-          country(billing_address), # billing_country
-          billing_address.zipcode, # billing_postcode
+          billing_address.province, # billing_region
+          billing_address.country, # billing_country
+          billing_address.zip, # billing_postcode
           billing_address.phone, # billing_telephone
-          '', # billing_company
+          billing_address.company, # billing_company
           '', # billing_fax
           '', # shipping_prefix
-          shipping_addres.firstname, # shipping_firstname
+          shipping_address.first_name, # shipping_firstname
           '', # shipping_middlename
-          shipping_addres.lastname, # shipping_lastname
+          shipping_address.last_name, # shipping_lastname
           '', # shipping_suffix
-          street(shipping_addres), # shipping_street_full
-          shipping_addres.city, # shipping_city
-          state(shipping_addres), # shipping_region
-          country(shipping_addres), # shipping_country
-          shipping_addres.zipcode, # shipping_postcode
-          shipping_addres.phone, # shipping_telephone
-          '', # shipping_company
+          street(shipping_address), # shipping_street_full
+          shipping_address.city, # shipping_city
+          shipping_address.province, # shipping_region
+          shipping_address.country, # shipping_country
+          shipping_address.zip, # shipping_postcode
+          shipping_address.phone, # shipping_telephone
+          shipping_address.company, # shipping_company
           '', # shipping_fax
           store.name, # created_in
-          0, # is_subscribed
-          nil, # customer_id
+          (order.buyer_accepts_marketing ? 1 : 0), # is_subscribed
+          (user.nil? ? nil : user.magento_id), # customer_id
           order.created_at, # created_at
           order.updated_at, # updated_at
           totals[:tax], # tax_amount
           totals[:shipping], # shipping_amount
-          0, # discount_amount
+          totals[:discount], # discount_amount
           totals[:subtotal], # subtotal
           totals[:grand], # grand_total
           totals[:paid], # total_paid
           totals[:refunded], # total_refunded
-          0, # total_qty_ordered
+          quantity_ordered(order), # total_qty_ordered
           totals[:canceled], # total_canceled
           totals[:grand], # total_invoiced
           totals[:refunded], # total_online_refunded
           0, # total_offline_refunded
           totals[:tax], # base_tax_amount
           totals[:shipping], # base_shipping_amount
-          0, # base_discount_amount
+          totals[:discount], # base_discount_amount
           totals[:subtotal], # base_subtotal
           totals[:grand], # base_grand_total
           totals[:paid], # base_total_paid
           totals[:refunded], # base_total_refunded
-          0, # base_total_qty_ordered
+          quantity_ordered(order), # base_total_qty_ordered
           totals[:canceled], # base_total_canceled
           totals[:grand], # base_total_invoiced
           totals[:refunded], # base_total_online_refunded
@@ -157,6 +157,117 @@ module Gemgento::Adapter::Shopify
           products_ordered(line_items, store), # products_ordered
           order_status(order.state) # order_status
       ]
+    end
+
+    # Retrieve the store related to the order.
+    #
+    # @param order [ShopifyAPI::Order]
+    # @return [Gemgento::Store]
+    def self.store(order)
+      return Gemgento::Store.first
+    end
+
+    # Calculate the various totals for an order.
+    #
+    # @param [ShopifyAPI::Order]
+    # @return [Hash(Double)]
+    def self.totals(order)
+      totals = {}
+      totals[:subtotal] = order.subtotal_price
+      totals[:tax] = order.total_tax
+      totals[:shipping] = shipping_amount(order)
+      totals[:discount] = order.total_discounts
+      totals[:grand] = order.total_price
+      totals[:refunded] = refund_amount(order)
+      totals[:paid] = paid_amount(order)
+      totals[:canceled] = canceled_amount(order)
+
+      return totals
+    end
+
+    # Get the user group from the provided user.  Default to NOT LOGGED IN.
+    #
+    # @param user [Gemgento::User, nil]
+    # @return [String]
+    def self.user_group(user)
+      if user.nil?
+        return Gemgento::UserGroup.find_by(magento_id: 0).code
+      else
+        return user.user_group.code
+      end
+    end
+
+    # Get a formatted street attribute from an address.
+    #
+    # @param address [ShopifyAPI::ShippingAddress, ShopifyAPI::BillingAddress]
+    # @return [String]
+    def self.street(address)
+      street = address.address1
+      street = "#{street}\n#{address.address2}" unless address.address2.blank?
+
+      return street
+    end
+
+    # Calculate total shipping cost for shopify order.
+    #
+    # @param order [ShopifyAPI::Order]
+    # @return [Double]
+    def self.shipping_amount(order)
+      total = 0.0
+
+      order.shipping_lines.each do |shipping_line|
+        total = total + shipping_line.price
+      end
+
+      return total
+    end
+
+    # Calculate the total amount refunded.
+    #
+    # @param order [ShopifyAPI::Order]
+    # @return [Double]
+    def self.refund_amount(order)
+      total = 0.0
+
+      order.refunds.each do |refund|
+        refund.transactions.each do |transaction|
+          total = total + refund.amount if transaction.kind = 'refund'
+        end
+      end
+
+      return total
+    end
+
+    # Calculate total quantity of ordered items.
+    #
+    # @param [ShopifyAPI::Order]
+    # @return [Double]
+    def self.quantity_ordered(order)
+      total = 0.0
+
+      return total
+    end
+
+    # Determine the Magento order status from a Shopify order.
+    #
+    # @param order [ShopifyAPI::Order]
+    # @return [String]
+    def self.order_status(order)
+      if %w[paid refunded partially_refunded].include? order.financial_status
+        if order.fulfillment_status == 'shipped'
+          return 'complete'
+        elsif %w[refunded partially_refunded].include? order.financial_status
+          return 'complete'
+        else
+          return 'pending'
+        end
+      elsif order.financial_status == 'pending'
+        return 'pending'
+      elsif order.financial_status == 'voided'
+        return 'canceled'
+      else
+        return 'complete'
+      end
     end
 
   end
