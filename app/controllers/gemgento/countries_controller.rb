c881cbe28fbbfe751b32364dcb13c654c42bfcc7
Kevin <kevin.pheasey@gmail.com>
2014-07-23 12:34:16 -0400

    Allo CountriesController to respond to all requests.

--- app/controllers/gemgento/countries_controller.rb
+++ app/controllers/gemgento/countries_controller.rb
@@ -1,18 +1,15 @@
 module Gemgento
   class CountriesController < Gemgento::ApplicationController
 
-    respond_to :json, :html
-
     def index
       @countries = Gemgento::Country.all.includes(:regions)
-
       respond_with @countries
     end
 
     def show
       @country = Gemgento::Country.find(params[:id])
-
       respond_with @country
     end
+
   end
 end
