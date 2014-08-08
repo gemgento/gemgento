c065c8c5d9c79545e8617af6888cfba4ceb9b543
Kevin <kevin.pheasey@gmail.com>
2014-08-08 12:20:41 -0400

    Add a named scope for each RecurringProfile state.

--- app/models/gemgento/recurring_profile.rb
+++ app/models/gemgento/recurring_profile.rb
@@ -16,6 +16,10 @@ module Gemgento
     serialize :profile_vendor_info, Hash
     serialize :additional_info, Hash
 
+    scope :active, -> { where(state: 'active') }
+    scope :suspended, -> { where(state: 'suspended') }
+    scope :canceled, -> { where(state: 'canceled') }
+
     def change_state(state)
       Gemgento::API::SOAP::Sales::RecurringProfile.update_state(self.magento_id, state)
     end
