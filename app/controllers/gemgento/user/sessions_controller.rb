f4a7a916e391ed5c24c336ac794d651bb71a51d5
Kevin <kevin.pheasey@gmail.com>
2014-08-28 11:26:41 -0400

    Convert Magento passwords before attempting a login.

--- app/controllers/gemgento/user/sessions_controller.rb
+++ app/controllers/gemgento/user/sessions_controller.rb
@@ -3,17 +3,17 @@ module Gemgento
     include SslRequirement
     ssl_required :new, :create, :destroy
     respond_to :html, :json
+    prepend_before_filter :convert_magento_password
 
     # POST /resource/sign_in
     def create
-      Gemgento::User.is_valid_login params[:user][:email], params[:user][:password]
       self.resource = warden.authenticate!(auth_options)
       set_flash_message(:notice, :signed_in) if is_flashing_format?
       sign_in(resource_name, resource)
       yield resource if block_given?
 
       respond_to do |format|
-        format.html { respond_with resource, :location => after_sign_in_path_for(resource) }
+        format.html { respond_with resource, location: after_sign_in_path_for(resource) }
         format.json do
           render json: {
               result: true,
@@ -22,9 +22,7 @@ module Gemgento
               csrfToken: form_authenticity_token
           }
         end
-
       end
-
     end
 
     # DELETE /resource/sign_out
@@ -42,6 +40,12 @@ module Gemgento
         format.all { head :no_content }
         format.any(*navigational_formats) { redirect_to redirect_path }
       end
+    end
+
+    private
+
+    def convert_magento_password
+      Gemgento::User.is_valid_login params[:user][:email], params[:user][:password]
     end
 
   end
