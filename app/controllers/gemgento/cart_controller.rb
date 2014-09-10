811211d31a47e34858f4c2b6790f567ecead84ea
Kevin <kevin.pheasey@gmail.com>
2014-09-10 10:07:13 -0400

    Cart#update accepts params for background worker processing

--- app/controllers/gemgento/cart_controller.rb
+++ app/controllers/gemgento/cart_controller.rb
@@ -125,7 +125,7 @@ module Gemgento
       if order_item.nil?
         if product.in_stock? params[:quantity], current_store
           # add the item to the order
-          result = current_order.add_item(product, params[:quantity], params[:options])
+          result = current_order.add_item(product, params[:quantity], params[:options], params[:background_worker])
           if result == true
             return product
           else
@@ -151,7 +151,7 @@ module Gemgento
 
       if product.in_stock? params[:quantity], current_store
         # update the item
-        result = current_order.update_item(product, params[:quantity], params[:options])
+        result = current_order.update_item(product, params[:quantity], params[:options], params[:background_worker])
         if result == true
           return product
         else
