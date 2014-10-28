686de3ffb8166aa19aee4c50d7ee6920e01054ec
Kevin <kevin.pheasey@gmail.com>
2014-10-28 12:36:10 -0400

    Create Quote model.

---
+++ app/models/gemgento/quote.rb
@@ -0,0 +1,22 @@
+module Gemgento
+  class Gemgento < ActiveRecord::Base
+    belongs_to :store, class_name: 'Gemgento::Store'
+    belongs_to :user, class_name: 'Gemgento::User'
+    belongs_to :user_group, class_name: 'Gemgento::UserGroup'
+    belongs_to :shipping_address, foreign_key: 'shipping_address_id', class_name: 'Gemgento::Address'
+    belongs_to :billing_address, foreign_key: 'billing_address_id', class_name: 'Gemgento::Address'
+
+    has_many :line_items, as: :itemizable
+    has_many :products, through: :line_items
+
+    has_one :payment, as: :payable
+
+    accepts_nested_attributes_for :billing_address
+    accepts_nested_attributes_for :shipping_address
+    accepts_nested_attributes_for :payment
+
+    attr_accessor :tax, :total, :push_cart_customer, :subscribe
+
+    validates :customer_email, format: /@/, allow_nil: true
+  end
+end
