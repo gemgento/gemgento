ba9fcb822cf975f58583a0ff9f466690a928fcc8
Kevin <kevin.pheasey@gmail.com>
2014-10-30 17:33:54 -0400

    Remove Payment validations since they are application specific.

--- app/models/gemgento/payment.rb
+++ app/models/gemgento/payment.rb
@@ -4,7 +4,6 @@ module Gemgento
 
     attr_accessor :cc_number, :cc_cid, :save_card, :payment_id
 
-    validates :cc_owner, :cc_type, :cc_exp_month, :cc_exp_year, :cc_cid, :cc_number, presence: true, if: 'payment_id.blank?'
     validates :payable, presence: true
   end
 end
