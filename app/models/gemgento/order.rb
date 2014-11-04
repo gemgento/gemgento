e5b71b8862ad7332c617b9347379a56e13362902
Kevin <kevin.pheasey@gmail.com>
2014-11-04 08:26:37 -0500

    Document Order

--- app/models/gemgento/order.rb
+++ app/models/gemgento/order.rb
@@ -16,22 +16,20 @@ module Gemgento
     has_one :billing_address, -> { where is_billing: true }, class_name: 'Address', as: :addressable
     has_one :shipping_address, -> { where is_shipping: true }, class_name: 'Address', as: :addressable
 
-    accepts_nested_attributes_for :billing_address
-    accepts_nested_attributes_for :shipping_address
-    accepts_nested_attributes_for :payment
-
     attr_accessor :tax, :total, :push_cart_customer, :subscribe
-
-    after_commit :subscribe_customer, if: :subscribe
-
-    serialize :cart_item_errors, Array
 
     validates :customer_email, format: /@/, allow_nil: true
 
+    # Return the increment_id instead of id.  This is for privacy purposes.
+    #
+    # @return [String]
     def to_param
       self.increment_id
     end
 
+    # Push the gift_message to Magento as a comment.
+    #
+    # @return [Void]
     def push_gift_message_comment
       API::SOAP::Sales::Order.add_comment(self.increment_id, self.status, "Gemgento Gift Message: #{self.gift_message}")
     end
