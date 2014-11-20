e50baa1a6060881ab0b907017bfab47410dad63d
Kevin <kevin.pheasey@gmail.com>
2014-11-20 12:04:26 -0500

    Add API calls to manage saved creidt cards.

--- lib/gemgento/api/soap/authnetcim/payment.rb
+++ lib/gemgento/api/soap/authnetcim/payment.rb
@@ -4,8 +4,38 @@ module Gemgento
       module Authnetcim
         class Payment
 
+          # Get a list of saved payment methods for customer.
+          #
+          # @param customer_id [Integer] Magento customer id.
+          # @return [Gemgento::MagentoResponse]
           def self.list(customer_id)
             Magento.create_call(:authnetcim_payment_list, { customer_id: customer_id })
+          end
+
+          # Create a new saved payment method for a customer
+          #
+          # @param customer_id [Integer] Magento customer id.
+          # @param payment [Hash] Payment parameters
+          # @return [Gemgento::MagentoResponse]
+          def self.create(customer_id, payment)
+            message = {
+                customer_id: customer_id,
+                payment: payment
+            }
+            Magento.create_call(:authnetcim_payment_create, message)
+          end
+
+          # Destroy a saved payment method for a customer.
+          #
+          # @param customer_id [Integer] Magento customer id.
+          # @param payment_profile_id [String]
+          # @return [Gemgento::MagentoResponse]
+          def self.destroy(customer_id, payment_profile_id)
+            message = {
+                customer_id: customer_id,
+                payment_profile_id: payment_profile_id
+            }
+            Magento.create_call(:authnetcim_payment_destroy, message)
           end
 
         end
