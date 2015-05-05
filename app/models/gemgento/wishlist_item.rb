f8ab00f18267885f6f624a307ba7c6e9a4e33140
Kevin Pheasey <kevin.pheasey@gmail.com>
2015-05-05 14:48:22 -0400

    Add presence and uniqueness validations to Gemgento::WishlistItem

--- app/models/gemgento/wishlist_item.rb
+++ app/models/gemgento/wishlist_item.rb
@@ -4,5 +4,8 @@ module Gemgento
   class WishlistItem < ActiveRecord::Base
     belongs_to :product
     belongs_to :user
+
+    validates :product, :user, presence: true
+    validates :product, uniqueness: { scope: :user }
   end
 end
