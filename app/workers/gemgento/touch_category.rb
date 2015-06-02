3f1ba843c8aec0a2a805d9d914e2c65e3b1d3418
Kevin Pheasey <kevin.pheasey@gmail.com>
2015-06-02 12:58:28 -0400

    Skip magento category save callbacks

--- app/workers/gemgento/touch_category.rb
+++ app/workers/gemgento/touch_category.rb
@@ -4,9 +4,9 @@ module Gemgento
     sidekiq_options backtrace: true
 
     def perform(category_ids)
-      Gemgento::Category.skip_callback(:save, :after, :sync_local_to_magento)
+      Gemgento::Category.skip_callback(:save, :before, :create_magento_category)
+      Gemgento::Category.skip_callback(:save, :before, :update_magento_category)
       Gemgento::Category.where(id: category_ids).each{ |c| c.update(updated_at: Time.now) }
-      Gemgento::Category.set_callback(:save, :after, :sync_local_to_magento)
     end
   end
 end
