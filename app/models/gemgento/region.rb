ca9c8aadf06b54aa796a40345471fe7bb4291b02
Kevin Pheasey <kevin.pheasey@gmail.com>
2015-06-04 16:29:36 -0400

    Region returns code if name is nil

--- app/models/gemgento/region.rb
+++ app/models/gemgento/region.rb
@@ -4,14 +4,10 @@ module Gemgento
   class Region < ActiveRecord::Base
     belongs_to :country
 
-    default_scope -> { order(:name) }
+    default_scope -> { order(:name, :code) }
 
-    def self.index
-      if Region.all.size == 0
-        API::SOAP::Directory::Region.fetch_all
-      end
-
-      Region.all
+    def name
+      super.nil? ? self.code : super
     end
 
   end
