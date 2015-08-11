441ce02c2eac4d5ff6ce0db416c6c0ae31bdbbba
Kevin Pheasey <kevin.pheasey@gmail.com>
2015-08-11 15:54:41 -0400

    Strip filter methods down to basics; set_filters and filter_options.

--- app/controllers/gemgento/categories_controller.rb
+++ app/controllers/gemgento/categories_controller.rb
@@ -1,6 +1,7 @@
 module Gemgento
   class CategoriesController < ApplicationController
 
+    before_filter :set_filters, only: :show
     after_filter :set_last_category, only: :show
 
     def index
@@ -10,10 +11,23 @@ module Gemgento
 
     def show
       @current_category = Gemgento::Category.active.find_by!('id = ? OR url_key = ?', params[:id], params[:id])
-      @products = current_category.products.active.catalog_visible.page(params[:page]).per(params[:per])
+      @products = current_category.products.active.catalog_visible
+      @products = @products.filter(@filters) unless @filters.empty?
+      @products = @products.page(params[:page]).per(params[:per])
     end
 
     private
+
+    # Get an array of all the active filters.
+    #
+    # @return [Array(Hash(:attribute, :value, :operand))]
+    def set_filters
+      @filters ||= begin
+        filters = []
+        filters << { attribute: Gemgento::ProductAttribute.find_by!(code: 'color'), value: params[:color] } unless params[:color].blank?
+        filters
+      end
+    end
 
     def set_last_category
       session[:category] = @current_category.id
--- app/helpers/gemgento/categories_helper.rb
+++ app/helpers/gemgento/categories_helper.rb
@@ -6,51 +6,15 @@ module Gemgento::CategoriesHelper
   # @param attribute [Symbol] Gemgento::Attribute code
   # @return [Array(String)]
   def filter_options(attribute)
-    options = []
+    filters = @filters.select{ |f| (f[:attribute].is_a?(Array) ? f[:attribute].first[:code] : f[:attribute].code) != attribute.to_s }
 
-    @current_category.children.active.each do |cat|
-      filters = filters.select{ |f| (f[:attribute].is_a?(Array) ? f[:attribute].first[:code] : f[:attribute].code) != attribute.to_s }
-      products = Gemgento::Product.active.catalog_visible
-                     .joins(:product_categories)
-                     .where('gemgento_product_categories.category_id = ?', cat.id)
-      products = products.filter(filters) if filters.any?
+    products = Gemgento::Product.active.catalog_visible
+                   .joins(:product_categories)
+                   .where('gemgento_product_categories.category_id = ?', @current_category.id)
+    products = products.filter(filters) if filters.any?
 
-      options = options + products.map(&attribute)
-    end
-
+    options = products.map(&attribute)
     options.uniq.reject(&:blank?).sort
-  end
-
-  # Get an array of all the active filters.
-  #
-  # @return [Array(Hash(:attribute, :value, :operand))]
-  def filters
-    @filters ||= begin
-      filters = []
-      filters << { attribute: Gemgento::ProductAttribute.find_by!(code: 'color'), value: params[:color] } unless params[:color].blank?
-      filters
-    end
-  end
-
-  # URL that includes the attribute, or attributes.  This will remove certain existing attributes if they are not relevant.
-  #
-  # @param attribute [Symbol] Gemgento::Attribute code
-  # @param value [Mixed]
-  # @param price_range [PriceRange]
-  # @return [String]
-  def filter_path(attribute = nil, value = nil, price_range = nil)
-    query_params = params.except(*request.path_parameters.keys)
-    query_params = query_params.merge(attribute => value) unless attribute.nil? || value.nil?
-
-    unless price_range.nil?
-      query_params = query_params.merge(price_min: price_range.min) unless price_range.min.nil?
-      query_params = query_params.merge(price_max: price_range.max) unless price_range.max.nil?
-    end
-
-    query_params.delete(:opacity) if query_params[:color_filter_primary] && query_params[:color_filter_primary].downcase != 'white'
-    query_params.delete(:color_filter_secondary) if query_params[:color_filter_primary] != params[:color_filter_primary]
-
-    "#{request.original_url.split('?').first}?#{query_params.to_query}"
   end
 
 end
