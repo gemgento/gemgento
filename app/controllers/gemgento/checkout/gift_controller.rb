0d7735ef415408115f4c7fd186da54931582a7ee
Kevin Pheasey <kevin@kpheasey.com>
2015-11-23 12:24:07 -0500

    Adding a gift message will redirect back instead of to a custom route.

--- app/controllers/gemgento/checkout/gift_controller.rb
+++ app/controllers/gemgento/checkout/gift_controller.rb
@@ -6,11 +6,11 @@ module Gemgento
       respond_to do |format|
 
         if @quote.update(quote_params)
-          format.html { redirect_to checkout_login_path }
-          format.json { render json: { result: true, order: @quote } }
+          format.html { redirect_to :back }
+          format.json { render json: { result: true } }
         else
-          format.html { redirect_to cart_path, alert: 'There was a problem saving the gift message.' }
-          format.json { render json: { result: false, errors: @quote.errors.full_messages } }
+          format.html { redirect_to :back, alert: 'There was a problem saving the gift message.' }
+          format.json { render json: { result: false, errors: @quote.errors.full_messages }, status: 422 }
         end
       end
     end
