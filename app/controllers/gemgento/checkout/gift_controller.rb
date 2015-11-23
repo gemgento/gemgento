fca32affec3d29ca90c40df00520d1ee017c7f12
Kevin Pheasey <kevin@kpheasey.com>
2015-11-23 17:08:07 -0500

    Add methods for defining after_gift_update paths.  Use old defaults.

--- app/controllers/gemgento/checkout/gift_controller.rb
+++ app/controllers/gemgento/checkout/gift_controller.rb
@@ -6,16 +6,24 @@ module Gemgento
       respond_to do |format|
 
         if @quote.update(quote_params)
-          format.html { redirect_to :back }
+          format.html { redirect_to after_gift_update_success_path }
           format.json { render json: { result: true } }
         else
-          format.html { redirect_to :back, alert: 'There was a problem saving the gift message.' }
+          format.html { redirect_to after_gift_update_fail_path, alert: 'There was a problem saving the gift message.' }
           format.json { render json: { result: false, errors: @quote.errors.full_messages }, status: 422 }
         end
       end
     end
 
     private
+
+    def after_gift_update_success_path
+      checkout_login_path
+    end
+
+    def after_gift_update_fail_path
+      cart_path
+    end
 
     def quote_params
       params.require(:quote).permit(:gift_message)
