1f6d14b0d7a2f1cd86d4b4fd2218b45c1a13f2ac
Kevin Pheasey <kevin@kpheasey.com>
2015-11-30 16:16:21 -0500

    Revert "add paperclip medium url by default to assets returned with api"
    This reverts commit 0502fbdeb7c5be94b99800d07e22b6ace50dc86d.

--- app/controllers/gemgento/api/v1/products_controller.rb
+++ app/controllers/gemgento/api/v1/products_controller.rb
@@ -4,16 +4,49 @@ module Gemgento
 
     def index
       if params[:category_id]
-        @products = Gemgento::Category.find(params[:category_id]).products(current_store).active.catalog_visible.order(:id)
+        @products = Gemgento::Category.find(params[:category_id]).products(current_store)
       else
-        @products = current_store.products.order(:id).active.catalog_visible
+        @products = current_store.products.order(:id)
       end
 
+      @products = @products.where(basic_filters)
+      @products = @products.filter(attribute_filters, current_store) if attribute_filters.any?
       @products = @products.page(@page[:number]).per(@page[:size])
     end
 
     def show
       @product = Gemgento::Product.find(params[:id])
+    end
+
+    protected
+
+    def basic_filters
+      default_filters.merge(filter_params)
+    end
+
+    def default_filters
+      {
+          status: 1, # enabled
+          visibility: [2, 4], # catalog visible
+      }
+    end
+
+    def attribute_filters
+      attribute_filters = []
+      all_attribute_codes = Gemgento::ProductAttribute.pluck(:code).uniq.map(&:to_sym)
+      filterable_attribute_codes = all_attribute_codes - default_filters.keys
+      attribute_filter_params = params.permit(filterable_attribute_codes)
+
+      attribute_filter_params.each do |code, value|
+        attribute = Gemgento::ProductAttribute.find_by!(code: code)
+        attribute_filters << { attribute: attribute, value: value }
+      end
+
+      return attribute_filters
+    end
+
+    def filter_params
+      params.permit(:status, :visibility, :magento_type, :sku)
     end
 
   end
