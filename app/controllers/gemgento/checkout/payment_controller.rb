ac5e7a7be94e794134d98eb575e4a78414b8f0d1
Kevin Pheasey <kevin@kpheasey.com>
2016-01-12 00:57:52 -0500

    Response with complete errors on checkout/payment#update

--- app/controllers/gemgento/checkout/payment_controller.rb
+++ app/controllers/gemgento/checkout/payment_controller.rb
@@ -37,7 +37,7 @@ module Gemgento
         else
           initialize_payment_variables
           format.html { render action: :show }
-          format.json { render json: { result: false, errors: @quote.errors.full_messages }, status: 422 }
+          format.json { render json: { result: false, errors: @quote.errors }, status: 422 }
         end
       end
     end
@@ -61,7 +61,7 @@ module Gemgento
         else
           initialize_payment_variables
           format.html { render action: :show }
-          format.json { render json: { result: false, errors: @quote.errors.full_messages }, status: 422 }
+          format.json { render json: { result: false, errors: @quote.errors }, status: 422 }
         end
       end
     end
