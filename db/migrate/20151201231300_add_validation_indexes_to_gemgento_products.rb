7777a8463169f597a24f4db532c2cdd731d88d04
Kevin Pheasey <kevin@kpheasey.com>
2016-01-22 14:40:36 -0500

    Update product validation indexes to remove older non unique magento ids

--- db/migrate/20151201231300_add_validation_indexes_to_gemgento_products.rb
+++ db/migrate/20151201231300_add_validation_indexes_to_gemgento_products.rb
@@ -3,4 +3,20 @@ class AddValidationIndexesToGemgentoProducts < ActiveRecord::Migration
     add_index :gemgento_products, :magento_id, unique: true
     add_index :gemgento_products, [:sku, :deleted_at], unique: true
   end
+
+  def migrate(direction)
+    if direction == :up
+      # remove duplicates on magento_id
+      products = Gemgento::Product.order(:deleted_at)
+      grouped = products.group_by{ |model| model.magento_id }
+
+      grouped.values.each do |duplicates|
+        next if duplicates.size == 1
+        original = duplicates.shift
+        duplicates.each { |dup| dup.destroy }
+      end
+    end
+
+    super
+  end
 end
